45befcb1351c93ca4aa66180577db3e145d55602
Josh Morman <mormjb@gmail.com>
2020-01-29 17:27:34 +0100

    tie vector source into the new work io structs

--- include/io_signature.hpp
+++ include/io_signature.hpp
@@ -9,7 +9,10 @@
 #define INCLUDED_IO_SIGNATURE_H
 
 #include <vector>
-#include <typeindex>      // std::type_index
+#include <iostream>
+#include <typeinfo>
+#include <typeindex>
+
 namespace gr {
 class io_signature_capability
 {
@@ -18,12 +21,13 @@ private:
     int d_min_streams;
     int d_max_streams;
     std::vector<int> d_sizeof_stream_item;
-    std::vector<std::type_index> d_type_index = std::vector<std::type_index>();
+    std::vector<std::type_index&> d_type_index;
 
 public:
     io_signature_capability(int min_streams,
                  int max_streams,
-                 const std::vector<int>& sizeof_stream_items);
+                 const std::vector<int>& sizeof_stream_items,
+                 std::vector<std::type_index&>& type_index);
 
     ~io_signature_capability(){}
 
@@ -35,11 +39,12 @@ class io_signature
 private:
     int d_n_streams;
     std::vector<int> d_sizeof_stream_item;
-    std::vector<std::type_index> d_type_index;
+    std::vector<std::type_index> d_type_info;
 
 public:
     io_signature(int n_streams,
-                 const std::vector<int>& sizeof_stream_items);
+                 const std::vector<int>& sizeof_stream_items,
+                 std::vector<std::type_index&>& type_index);
 
     ~io_signature(){}
 
