c0c72ed91d482c28230a9a92605ab7da571a402d
Josh Morman <mormjb@gmail.com>
2020-01-29 17:39:05 +0100

    clang_tidy on vector_source

--- include/vector_source.hpp
+++ include/vector_source.hpp
@@ -5,39 +5,45 @@
 
 namespace gr {
 namespace blocks {
-template <class T> class vector_source : virtual public sync_block {
+template <class T>
+class vector_source : virtual public sync_block
+{
 private:
-  /*   Get rid of this probably  */
-  // Publish what used to be in the input signature -- does this go into the 
-  // This is all very ugly right now - punt for now on publishing this information
-  static const int MIN_INPUT_STREAMS = 0;
-  static const int MAX_INPUT_STREAMS = 0;
-  static const int MIN_OUTPUT_STREAMS = 1;
-  static const int MAX_OUTPUT_STREAMS = -1;
+    /*   Get rid of this probably  */
+    // Publish what used to be in the input signature -- does this go into the
+    // This is all very ugly right now - punt for now on publishing this information
+    static const int MIN_INPUT_STREAMS = 0;
+    static const int MAX_INPUT_STREAMS = 0;
+    static const int MIN_OUTPUT_STREAMS = 1;
+    static const int MAX_OUTPUT_STREAMS = -1;
 
-  static const io_signature_capability d_input_signature_capability = io_signature_capability(MAX_INPUT_STREAMS, MAX_INPUT_STREAMS);
-  static const io_signature_capability d_output_signature_capability = io_signature_capability(MIN_OUTPUT_STREAMS, MAX_OUTPUT_STREAMS);
-  /* ------------------------   */
+    static const io_signature_capability d_input_signature_capability =
+        io_signature_capability(MAX_INPUT_STREAMS, MAX_INPUT_STREAMS);
+    static const io_signature_capability d_output_signature_capability =
+        io_signature_capability(MIN_OUTPUT_STREAMS, MAX_OUTPUT_STREAMS);
+    /* ------------------------   */
 
-  std::vector<T> d_data;
-  bool d_repeat;
-  unsigned int d_offset;
-  unsigned int d_vlen;
-  bool d_settags;
-  std::vector<tag_t> d_tags;
+    std::vector<T> d_data;
+    bool d_repeat;
+    unsigned int d_offset;
+    unsigned int d_vlen;
+    bool d_settags;
+    std::vector<tag_t> d_tags;
 
 public:
-  vector_source(const std::vector<T> &data, bool repeat, unsigned int vlen,
-                const std::vector<tag_t> &tags);
-  ~vector_source();
+    vector_source(const std::vector<T>& data,
+                  bool repeat,
+                  unsigned int vlen,
+                  const std::vector<tag_t>& tags);
+    ~vector_source();
 
-  virtual work_return_code_t work(std::vector<block_work_input> &work_input,
-                                  std::vector<block_work_output> &work_output);
+    virtual work_return_code_t work(std::vector<block_work_input>& work_input,
+                                    std::vector<block_work_output>& work_output);
 
-  void rewind();
-  void set_data(const std::vector<T> &data,
-                const std::vector<tag_t> &tags = std::vector<tag_t>());
-  void set_repeat(bool repeat);
+    void rewind();
+    void set_data(const std::vector<T>& data,
+                  const std::vector<tag_t>& tags = std::vector<tag_t>());
+    void set_repeat(bool repeat);
 };
 
 typedef vector_source<std::uint8_t> vector_source_b;
--- lib/vector_source.cc
+++ lib/vector_source.cc
@@ -20,101 +20,113 @@ namespace gr {
 namespace blocks {
 
 template <class T>
-vector_source<T>::vector_source(const std::vector<T> &data, bool repeat,
+vector_source<T>::vector_source(const std::vector<T>& data,
+                                bool repeat,
                                 unsigned int vlen,
-                                const std::vector<tag_t> &tags)
-    : sync_block("vector_source", io_signature::make(0, 0, 0),
+                                const std::vector<tag_t>& tags)
+    : sync_block("vector_source",
+                 io_signature::make(0, 0, 0),
                  io_signature::make(1, 1, sizeof(T) * vlen)),
-      d_data(data), d_repeat(repeat), d_offset(0), d_vlen(vlen), d_tags(tags) {
-  if (tags.empty()) {
-    d_settags = 0;
-  } else {
-    d_settags = 1;
-    this->set_output_multiple(data.size() / vlen);
-  }
-  if ((data.size() % vlen) != 0)
-    throw std::invalid_argument("data length must be a multiple of vlen");
-}
-
-template <class T>
-void vector_source<T>::set_data(const std::vector<T> &data,
-                                const std::vector<tag_t> &tags) {
-  d_data = data;
-  d_tags = tags;
-  rewind();
-  if (tags.empty()) {
-    d_settags = false;
-  } else {
-    d_settags = true;
-  }
-}
-
-template <class T>
-work_return_code_t
-vector_source<T>::work(std::vector<block_work_input> &work_input,
-                       std::vector<block_work_output> &work_output) {
-
-  size_t noutput_ports = work_output.size(); // is 1 for this block
-  noutput_items = work_output[0].n_items;
-  void *output_items = work_output[0].items;
-  std::vector<tag_t> output_tags = work_output[0].tags;
-  uint64_t n_written = work_output[0].n_items_written;
-
-  T *optr = (T *)output_items[0];
-
-  if (d_repeat) {
-    unsigned int size = d_data.size();
-    unsigned int offset = d_offset;
-    if (size == 0)
-      return -1;
-
-    if (d_settags) {
-      int n_outputitems_per_vector = d_data.size() / d_vlen;
-      for (int i = 0; i < noutput_items; i += n_outputitems_per_vector) {
-        // FIXME do proper vector copy
-        memcpy((void *)optr, (const void *)&d_data[0], size * sizeof(T));
-        optr += size;
-        for (unsigned t = 0; t < d_tags.size(); t++) {
-            
-        //   this->add_item_tag(0, this->nitems_written(0) + i + d_tags[t].offset,
-        //                      d_tags[t].key, d_tags[t].value, d_tags[t].srcid);
-        output_tags.push_back(tag_t(n_written + i + d_tags[t].offset, d_tags[t].key, d_tags[t].value, d_tags[t].srcid));
-
-        }
-      }
+      d_data(data),
+      d_repeat(repeat),
+      d_offset(0),
+      d_vlen(vlen),
+      d_tags(tags)
+{
+    if (tags.empty()) {
+        d_settags = 0;
     } else {
-      for (int i = 0; i < static_cast<int>(noutput_items * d_vlen); i++) {
-        optr[i] = d_data[offset++];
-        if (offset >= size) {
-          offset = 0;
+        d_settags = 1;
+        this->set_output_multiple(data.size() / vlen);
+    }
+    if ((data.size() % vlen) != 0)
+        throw std::invalid_argument("data length must be a multiple of vlen");
+}
+
+template <class T>
+void vector_source<T>::set_data(const std::vector<T>& data,
+                                const std::vector<tag_t>& tags)
+{
+    d_data = data;
+    d_tags = tags;
+    rewind();
+    if (tags.empty()) {
+        d_settags = false;
+    } else {
+        d_settags = true;
+    }
+}
+
+template <class T>
+work_return_code_t vector_source<T>::work(std::vector<block_work_input>& work_input,
+                                          std::vector<block_work_output>& work_output)
+{
+
+    size_t noutput_ports = work_output.size(); // is 1 for this block
+    noutput_items = work_output[0].n_items;
+    void* output_items = work_output[0].items;
+    std::vector<tag_t> output_tags = work_output[0].tags;
+    uint64_t n_written = work_output[0].n_items_written;
+
+    T* optr = (T*)output_items[0];
+
+    if (d_repeat) {
+        unsigned int size = d_data.size();
+        unsigned int offset = d_offset;
+        if (size == 0)
+            return -1;
+
+        if (d_settags) {
+            int n_outputitems_per_vector = d_data.size() / d_vlen;
+            for (int i = 0; i < noutput_items; i += n_outputitems_per_vector) {
+                // FIXME do proper vector copy
+                memcpy((void*)optr, (const void*)&d_data[0], size * sizeof(T));
+                optr += size;
+                for (unsigned t = 0; t < d_tags.size(); t++) {
+
+                    //   this->add_item_tag(0, this->nitems_written(0) + i +
+                    //   d_tags[t].offset,
+                    //                      d_tags[t].key, d_tags[t].value,
+                    //                      d_tags[t].srcid);
+                    output_tags.push_back(tag_t(n_written + i + d_tags[t].offset,
+                                                d_tags[t].key,
+                                                d_tags[t].value,
+                                                d_tags[t].srcid));
+                }
+            }
+        } else {
+            for (int i = 0; i < static_cast<int>(noutput_items * d_vlen); i++) {
+                optr[i] = d_data[offset++];
+                if (offset >= size) {
+                    offset = 0;
+                }
+            }
         }
-      }
+
+        d_offset = offset;
+
+        work_output[0].n_produced = noutput_items;
+        return work_return_code_t::WORK_OK;
+
+    } else {
+        if (d_offset >= d_data.size())
+            return -1; // Done!
+
+        unsigned n = std::min((unsigned)d_data.size() - d_offset,
+                              (unsigned)noutput_items * d_vlen);
+        for (unsigned i = 0; i < n; i++) {
+            optr[i] = d_data[d_offset + i];
+        }
+        for (unsigned t = 0; t < d_tags.size(); t++) {
+            if ((d_tags[t].offset >= d_offset) && (d_tags[t].offset < d_offset + n))
+                this->add_item_tag(
+                    0, d_tags[t].offset, d_tags[t].key, d_tags[t].value, d_tags[t].srcid);
+        }
+        d_offset += n;
+
+        work_output[0].n_produced = n / d_vlen;
+        return work_return_code_t::WORK_OK;
     }
-
-    d_offset = offset;
-
-    work_output[0].n_produced = noutput_items;
-    return work_return_code_t::WORK_OK;
-
-  } else {
-    if (d_offset >= d_data.size())
-      return -1; // Done!
-
-    unsigned n = std::min((unsigned)d_data.size() - d_offset,
-                          (unsigned)noutput_items * d_vlen);
-    for (unsigned i = 0; i < n; i++) {
-      optr[i] = d_data[d_offset + i];
-    }
-    for (unsigned t = 0; t < d_tags.size(); t++) {
-      if ((d_tags[t].offset >= d_offset) && (d_tags[t].offset < d_offset + n))
-        this->add_item_tag(0, d_tags[t].offset, d_tags[t].key, d_tags[t].value,
-                           d_tags[t].srcid);
-    }
-    d_offset += n;
-
-    work_output[0].n_produced = n / d_vlen;
-    return work_return_code_t::WORK_OK;
-  }
 }
 
 template class vector_source<std::uint8_t>;
