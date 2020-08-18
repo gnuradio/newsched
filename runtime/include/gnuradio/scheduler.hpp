216e2eaeae612a9cea34b1234693b3fc4730f1e1
mormj <mormjb@gmail.com>
2020-08-18 16:26:34 -0400

    runtime: add logging and preferences

--- runtime/include/gnuradio/scheduler.hpp
+++ runtime/include/gnuradio/scheduler.hpp
@@ -8,6 +8,7 @@
 
 #include <gnuradio/callback.hpp>
 #include <gnuradio/flat_graph.hpp>
+#include <gnuradio/logging.hpp>
 
 namespace gr {
 
@@ -31,7 +32,12 @@ public:
     param_action_queue param_query_queue;
     block_callback_queue callback_queue;
 
-    scheduler(const std::string& name) { _name = name; };
+    scheduler(const std::string& name)
+    {
+        _name = name;
+        _logger = logging::get_logger(name, "default");
+        _debug_logger = logging::get_logger(name + "_dbg", "debug");
+    };
     virtual ~scheduler();
     std::shared_ptr<scheduler> base() { return shared_from_this(); }
     virtual void initialize(flat_graph_sptr fg) = 0;
@@ -43,6 +49,7 @@ public:
                                          param_action_sptr param_action,
                                          param_action_complete_fcn cb_when_complete)
     {
+        gr_log_debug(_debug_logger,"request_parameter_query: {}", block_alias);
         param_query_queue.emplace(param_action_base_with_callback{
             block_alias, param_action, cb_when_complete });
     }
@@ -71,7 +78,11 @@ public:
     int id() { return _id; }
     void set_id(int id) { _id = id; }
     scheduler_state state() { return _state; }
-    void set_state (scheduler_state state) { _state = state; }
+    void set_state(scheduler_state state) { _state = state; }
+
+protected:
+    logger_sptr _logger;
+    logger_sptr _debug_logger;
 
 private:
     std::string _name;
