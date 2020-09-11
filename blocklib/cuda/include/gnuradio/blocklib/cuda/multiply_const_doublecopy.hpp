4e7b223f8771e2a2b845b78cabae7a02c7dcab15
mormj <mormjb@gmail.com>
2020-09-11 08:58:42 -0400

    blocks/cuda: rename multiply_const blocks to be more clear

--- blocklib/cuda/include/gnuradio/blocklib/cuda/multiply_const_doublecopy.hpp
+++ blocklib/cuda/include/gnuradio/blocklib/cuda/multiply_const_doublecopy.hpp
@@ -5,15 +5,15 @@
 namespace gr {
 namespace cuda {
 template <class T>
-class multiply_const2 : public sync_block
+class multiply_const_doublecopy : public sync_block
 {
 public:
     enum params : uint32_t { id_k, id_vlen, num_params };
 
-    typedef std::shared_ptr<multiply_const2> sptr;
+    typedef std::shared_ptr<multiply_const_doublecopy> sptr;
     static sptr make(const T k, const size_t vlen = 1)
     {
-        auto ptr = std::make_shared<multiply_const2>(multiply_const2<T>());
+        auto ptr = std::make_shared<multiply_const_doublecopy>(multiply_const_doublecopy<T>());
 
         ptr->add_port(port<T>::make("input",
                                     port_direction_t::INPUT,
@@ -25,18 +25,18 @@ public:
                                     std::vector<size_t>{ vlen }));
 
         ptr->add_param(
-            param<T>::make(multiply_const2<T>::params::id_k, "k", k, &ptr->d_k));
+            param<T>::make(multiply_const_doublecopy<T>::params::id_k, "k", k, &ptr->d_k));
 
         // TODO: vlen should be const and unchangeable as a parameter
         ptr->add_param(param<size_t>::make(
-            multiply_const2<T>::params::id_vlen, "vlen", vlen, &ptr->d_vlen));
+            multiply_const_doublecopy<T>::params::id_vlen, "vlen", vlen, &ptr->d_vlen));
 
         return ptr;
 
     }
-    multiply_const2()
-    : sync_block("multiply_const2 (cuda)") {}
-    // ~multiply_const2() {};
+    multiply_const_doublecopy()
+    : sync_block("multiply_const_doublecopy (cuda)") {}
+    // ~multiply_const_doublecopy() {};
 
     virtual work_return_code_t work(std::vector<block_work_input>& work_input,
                                     std::vector<block_work_output>& work_output);
@@ -53,7 +53,7 @@ private:
 
     };
 
-    typedef multiply_const2<float> multiply_const2_ff;
+    typedef multiply_const_doublecopy<float> multiply_const_doublecopy_ff;
 
 } // namespace blocks
 } // namespace blocks
