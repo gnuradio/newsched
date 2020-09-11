7a1e6505a9f16c674fc35207cd4b74f79885bad4
mormj <mormjb@gmail.com>
2020-09-11 08:58:42 -0400

    blocks/cuda: rename multiply_const blocks to be more clear

--- blocklib/cuda/lib/multiply_const.cu
+++ blocklib/cuda/lib/multiply_const.cu
@@ -2,7 +2,7 @@
 #include <cuda_runtime.h>
 
 __global__ 
-void multiply_const_kernel(int n, float a, float* in, float* out)
+void multiply_const_kernel(int n, float a, const float* in, float* out)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < n)
@@ -13,23 +13,10 @@ namespace gr {
 namespace cuda {
 void multiply_const_kernel_wrapper(int N, float k, const float* in, float* out)
 {
-    float *dev_x, *dev_y;
-
-
-    cudaMalloc(&dev_x, N * sizeof(float));
-    cudaMalloc(&dev_y, N * sizeof(float));
-
-
-    cudaMemcpy(dev_x, in, N * sizeof(float), cudaMemcpyHostToDevice);
-
     const int nthreads = 64;
     // Perform SAXPY on 1M elements
-    multiply_const_kernel<<<(N + nthreads - 1) / nthreads, nthreads>>>(N, k, dev_x, dev_y);
+    multiply_const_kernel<<<(N + nthreads - 1) / nthreads, nthreads>>>(N, k, in, out);
 
-    cudaMemcpy(out, dev_y, N * sizeof(float), cudaMemcpyDeviceToHost);
-
-    cudaFree(dev_x);
-    cudaFree(dev_y);
 }
 
 } // namespace cuda
