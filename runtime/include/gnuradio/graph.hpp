b60ceeef7b8d17f359218e78f3ec513525bd15ee
mormj <mormjb@gmail.com>
2020-11-16 12:59:39 -0500

    runtime: separate flowgraph partitioning as separate function

--- runtime/include/gnuradio/graph.hpp
+++ runtime/include/gnuradio/graph.hpp
@@ -2,8 +2,22 @@
 
 #include "api.h"
 #include <gnuradio/edge.hpp>
+// #include <gnuradio/domain.hpp>
+// #include <gnuradio/domain_adapter.hpp>
 
 namespace gr {
+
+template <class T>
+static std::vector<T> unique_vector(std::vector<T> v)
+{
+    std::vector<T> result;
+    std::insert_iterator<std::vector<T>> inserter(result, result.begin());
+
+    sort(v.begin(), v.end());
+    unique_copy(v.begin(), v.end(), inserter);
+    return result;
+}
+
 
 /**
  * @brief Represents a set of ports connected by edges
@@ -27,54 +41,13 @@ public:
     void connect(const node_endpoint& src,
                  const node_endpoint& dst,
                  buffer_factory_function buffer_factory = nullptr,
-                 std::shared_ptr<buffer_properties> buf_properties = nullptr)
-    {
-        // TODO: Do a bunch of checking
-
-        _edges.push_back(edge(src, dst, buffer_factory, buf_properties));
-        auto used_nodes = calc_used_nodes();
-        // used_nodes.insert(used_nodes.end(), _orphan_nodes.begin(),
-        // _orphan_nodes.end());
-        _nodes = used_nodes;
-
-        std::map<std::string, int> name_count;
-        // update the block alias
-        for (auto& b : _nodes) {
-            // look in the map, see how many of that name exist
-            // make the alias name + count;
-            // increment the map
-            int cnt;
-            if (name_count.find(b->name()) == name_count.end()) {
-                name_count[b->name()] = cnt = 0;
-            } else {
-                cnt = name_count[b->name()];
-            }
-            b->set_alias(b->name() + std::to_string(cnt));
-            name_count[b->name()] = cnt + 1;
-        }
-    }
+                 std::shared_ptr<buffer_properties> buf_properties = nullptr);
     void connect(node_sptr src_node,
                  unsigned int src_port_index,
                  node_sptr dst_node,
                  unsigned int dst_port_index,
                  buffer_factory_function buffer_factory = nullptr,
-                 std::shared_ptr<buffer_properties> buf_properties = nullptr)
-    {
-        port_sptr src_port = src_node->get_port(
-            src_port_index, port_type_t::STREAM, port_direction_t::OUTPUT);
-        if (src_port == nullptr)
-            throw std::invalid_argument("Source Port not found");
-
-        port_sptr dst_port = dst_node->get_port(
-            dst_port_index, port_type_t::STREAM, port_direction_t::INPUT);
-        if (dst_port == nullptr)
-            throw std::invalid_argument("Destination port not found");
-
-        connect(node_endpoint(src_node, src_port),
-                node_endpoint(dst_node, dst_port),
-                buffer_factory,
-                buf_properties);
-    }
+                 std::shared_ptr<buffer_properties> buf_properties = nullptr);
     void connect(node_sptr src_node,
                  std::string& src_port_name,
                  node_sptr dst_node,
@@ -82,53 +55,11 @@ public:
     void disconnect(const node_endpoint& src, const node_endpoint& dst){};
     virtual void validate(){};
     virtual void clear(){};
-    void add_orphan_node(node_sptr orphan_node) { _orphan_nodes.push_back(orphan_node); }
-    // /**
-    //  * @brief Return a flattened graph (all subgraphs reduced to their constituent
-    //  blocks
-    //  * and edges)
-    //  *
-    //  * @return graph
-    //  */
-    // flat_graph_sptr flatten()
-    // {
-    //     // For now we assume the graph is already flattened and we just downcast
-    //     everything
-
+    void add_orphan_node(node_sptr orphan_node);
 
     // }
-    node_vector_t calc_used_nodes()
-    {
-        node_vector_t tmp;
-
-        // Collect all blocks in the edge list
-        for (edge_viter_t p = _edges.begin(); p != _edges.end(); p++) {
-            tmp.push_back(p->src().node());
-            tmp.push_back(p->dst().node());
-        }
-        for (auto n : _orphan_nodes) {
-            tmp.push_back(n);
-        }
-
-        return unique_vector<node_sptr>(tmp);
-    }
-
-    std::vector<edge> find_edge(port_sptr port)
-    {
-        std::vector<edge> ret;
-        for (auto& e : edges()) {
-            if (e.src().port() == port)
-                ret.push_back(e);
-
-            if (e.dst().port() == port)
-                ret.push_back(e);
-        }
-
-        if (ret.empty())
-            throw std::invalid_argument("edge not found");
-
-        return ret;
-    }
+    node_vector_t calc_used_nodes();
+    std::vector<edge> find_edge(port_sptr port);
 };
 
 typedef std::shared_ptr<graph> graph_sptr;
--- runtime/lib/flowgraph.cpp
+++ runtime/lib/flowgraph.cpp
@@ -1,4 +1,5 @@
 #include <gnuradio/flowgraph.hpp>
+#include <gnuradio/graph_utils.hpp>
 
 namespace gr {
 
@@ -36,184 +37,17 @@ void flowgraph::partition(std::vector<domain_conf>& confs)
 {
     d_fgmon = std::make_shared<flowgraph_monitor>(d_schedulers);
     // Create new subgraphs based on the partition configuration
-    d_subgraphs.clear();
 
-    // std::vector<std::tuple<graph,edge>> domain_crossings;
-    std::vector<edge> domain_crossings;
-    std::vector<domain_conf> crossing_confs;
-    std::vector<scheduler_sptr> partition_scheds;
-    std::vector<scheduler_sptr> crossing_scheds;
-
-    std::map<nodeid_t, scheduler_sptr> block_to_scheduler_map;
-
-    for (auto& conf : confs) {
-
-        auto g = graph::make(); // create a new subgraph
-        // Go through the blocks assigned to this scheduler
-        // See whether they connect to the same graph or account for a domain crossing
-
-        auto sched = conf.sched();   // std::get<0>(conf);
-        auto blocks = conf.blocks(); // std::get<1>(conf);
-        for (auto b : blocks)        // for each of the blocks in the tuple
-        {
-            // Store the block to scheduler mapping for later use
-            block_to_scheduler_map[b->id()] = sched;
-
-            for (auto input_port : b->input_stream_ports()) {
-                auto edges = find_edge(input_port);
-                // There should only be one edge connected to an input port
-                // Crossings associated with the downstream port
-                auto e = edges[0];
-                auto other_block = e.src().node();
-
-                // Is the other block in our current partition
-                if (std::find(blocks.begin(), blocks.end(), other_block) !=
-                    blocks.end()) {
-                    g->connect(e.src(), e.dst(), e.buffer_factory(), e.buf_properties());
-                } else {
-                    // add this edge to the list of domain crossings
-                    // domain_crossings.push_back(std::make_tuple(g,e));
-                    domain_crossings.push_back(e);
-                    crossing_confs.push_back(conf);
-                }
-            }
-        }
-
-        d_subgraphs.push_back(g);
-        partition_scheds.push_back(sched);
-    }
-
-    int idx = 0;
-    for (auto& conf : confs) {
-        auto g = d_subgraphs[idx];
-
-        // see that all the blocks in conf->blocks() are in g, and if not, add them as
-        // orphan nodes
-
-        for (auto b : conf.blocks()) // for each of the blocks in the tuple
-        {
-            bool connected = false;
-            for (auto e : g->edges()) {
-                if (e.src().node() == b || e.dst().node() == b) {
-                    connected = true;
-                    break;
-                }
-            }
-
-            if (!connected) {
-                g->add_orphan_node(b);
-            }
-        }
-
-        idx++;
-    }
-
-    // Now, let's set up domain adapters at the domain crossings
-    // Several assumptions are being made now:
-    //   1.  All schedulers running on the same processor
-    //   2.  Outputs that cross domains can only be mapped one input
-    //   3.  Fixed client/server relationship - limited configuration of DA
-
-    std::map<scheduler_sptr, std::map<nodeid_t, neighbor_scheduler_info>>
-        neighbor_map_per_scheduler;
-
-    int crossing_index = 0;
-    for (auto c : domain_crossings) {
-        // Attach a domain adapter to the src and dst ports of the edge
-        // auto g = std::get<0>(c);
-        // auto e = std::get<1>(c);
-
-        // Find the subgraph that holds src block
-        graph_sptr src_block_graph = nullptr;
-        for (auto g : d_subgraphs) {
-            auto blocks = g->calc_used_nodes();
-            if (std::find(blocks.begin(), blocks.end(), c.src().node()) != blocks.end()) {
-                src_block_graph = g;
-                break;
-            }
-        }
-
-        // Find the subgraph that holds dst block
-        graph_sptr dst_block_graph = nullptr;
-        for (auto g : d_subgraphs) {
-            auto blocks = g->calc_used_nodes();
-            if (std::find(blocks.begin(), blocks.end(), c.dst().node()) != blocks.end()) {
-                dst_block_graph = g;
-                break;
-            }
-        }
-
-        if (!src_block_graph || !dst_block_graph) {
-            throw std::runtime_error("Cannot find both sides of domain adapter");
-        }
-
-        // Create Domain Adapter pair
-        // right now, only one port - have a list of available ports
-        // put the buffer downstream
-        auto conf = crossing_confs[crossing_index];
-
-        // Does the crossing have a specific domain adapter defined?
-        domain_adapter_conf_sptr da_conf = nullptr;
-        for (auto ec : conf.da_edge_confs()) {
-            auto conf_edge = std::get<0>(ec);
-            if (c == conf_edge) {
-                da_conf = std::get<1>(ec);
-                break;
-            }
-        }
-
-        // else if defined: use the default defined for the domain
-        if (!da_conf) {
-            da_conf = conf.da_conf();
-        }
-
-        // else, use the default domain adapter configuration ??
-        // TODO
-
-        // use the conf to produce the domain adapters
-        auto da_pair = da_conf->make_domain_adapter_pair(
-            c.src().port(),
-            c.dst().port(),
-            "da_" + c.src().node()->alias() + "->" + c.dst().node()->alias());
-        auto da_src = std::get<0>(da_pair);
-        auto da_dst = std::get<1>(da_pair);
-
-
-        // da_src->test();
-
-        // Attach domain adapters to the src and dest blocks
-        // domain adapters only have one port
-        src_block_graph->connect(c.src(),
-                                 node_endpoint(da_src, da_src->all_ports()[0]),
-                                 c.buffer_factory(),
-                                 c.buf_properties());
-        dst_block_graph->connect(node_endpoint(da_dst, da_dst->all_ports()[0]),
-                                 c.dst(),
-                                 c.buffer_factory(),
-                                 c.buf_properties());
-
-
-        // Set the block id to "other scheduler" maps
-        // This can/should be scheduler adapters, but use direct scheduler sptrs for now
-
-        auto dst_block_id = c.dst().node()->id();
-        auto src_block_id = c.src().node()->id();
-
-        neighbor_map_per_scheduler[block_to_scheduler_map[dst_block_id]][dst_block_id]
-            .set_upstream(block_to_scheduler_map[src_block_id], src_block_id);
-
-        neighbor_map_per_scheduler[block_to_scheduler_map[src_block_id]][src_block_id]
-            .add_downstream(block_to_scheduler_map[dst_block_id], dst_block_id);
-
-        crossing_index++;
-    }
+    auto graph_part_info = graph_utils::partition(base(), d_schedulers, confs);
 
     d_flat_subgraphs.clear();
-    for (size_t i = 0; i < partition_scheds.size(); i++) {
-        d_flat_subgraphs.push_back(flat_graph::make_flat(d_subgraphs[i]));
-        partition_scheds[i]->initialize(d_flat_subgraphs[i],
-                                        d_fgmon,
-                                        neighbor_map_per_scheduler[partition_scheds[i]]);
+    for (size_t i = 0; i < graph_part_info.subgraphs.size(); i++) {
+        d_flat_subgraphs.push_back(flat_graph::make_flat(graph_part_info.subgraphs[i]));
+        graph_part_info.partition_scheds[i]->initialize(
+            d_flat_subgraphs[i],
+            d_fgmon,
+            graph_part_info
+                .neighbor_map_per_scheduler[graph_part_info.partition_scheds[i]]);
     }
 }
 
