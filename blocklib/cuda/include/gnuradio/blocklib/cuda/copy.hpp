62d5d1d97d636f2038a660825841fb62df414f40
Josh Morman <jmorman@perspectalabs.com>
2021-02-08 14:47:25 -0500

    cuda: add streams to cuda blocks and buffers

--- blocklib/cuda/include/gnuradio/blocklib/cuda/copy.hpp
+++ blocklib/cuda/include/gnuradio/blocklib/cuda/copy.hpp
@@ -2,6 +2,9 @@
 
 #include <gnuradio/sync_block.hpp>
 
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+
 namespace gr {
 namespace cuda {
 
@@ -11,21 +14,19 @@ public:
     enum params : uint32_t { num_params };
 
     typedef std::shared_ptr<copy> sptr;
-    static sptr make(const size_t batch_size = 1)
+    static sptr make(const size_t batch_size = 1, const size_t load = 1)
     {
-        auto ptr =
-            std::make_shared<copy>(batch_size);
+        auto ptr = std::make_shared<copy>(batch_size, load);
 
         ptr->add_port(port<gr_complex>::make(
-            "input", port_direction_t::INPUT, port_type_t::STREAM, {batch_size}));
+            "input", port_direction_t::INPUT, port_type_t::STREAM, { batch_size }));
         ptr->add_port(port<gr_complex>::make(
-            "output", port_direction_t::OUTPUT, port_type_t::STREAM, {batch_size}));
+            "output", port_direction_t::OUTPUT, port_type_t::STREAM, { batch_size }));
 
         return ptr;
     }
 
-    copy(
-        const size_t batch_size);
+    copy(const size_t batch_size, const size_t load);
     ~copy();
 
 
@@ -34,9 +35,10 @@ public:
 
 private:
     size_t d_batch_size;
+    size_t d_load;
     int d_block_size;
     int d_min_grid_size;
-
+    cudaStream_t stream;
 };
 
 } // namespace cuda
