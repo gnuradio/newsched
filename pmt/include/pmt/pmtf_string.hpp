6e642eae53785775a534c5a620880142091a8daf
Josh Morman <jmorman@perspectalabs.com>
2021-02-17 08:03:18 -0500

    pmt: flatbuffer based pmt

---
+++ pmt/include/pmt/pmtf_string.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <pmt/pmt_generated.h>
+#include <pmt/pmtf.hpp>
+#include <complex>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <typeindex>
+#include <typeinfo>
+
+
+namespace pmtf {
+
+class pmt_string : public pmt_base
+{
+public:
+    typedef std::shared_ptr<pmt_string> sptr;
+    static sptr make(const std::string& value)
+    {
+        return std::make_shared<pmt_string>(value);
+    }
+    static sptr from_buffer(const uint8_t* buf)
+    {
+        return std::make_shared<pmt_string>(buf);
+    }
+    static sptr from_pmt(const pmtf::Pmt *fb_pmt)
+    {
+        return std::make_shared<pmt_string>(fb_pmt);
+    }
+
+
+    void set_value(const std::string& val);
+    std::string value();
+
+    void operator=(const std::string& other) // copy assignment
+    {
+        set_value(other);
+    }
+
+    bool operator==(const std::string& other) { return other == value(); }
+    bool operator!=(const std::string& other) { return other != value(); }
+
+    flatbuffers::Offset<void> rebuild_data(flatbuffers::FlatBufferBuilder& fbb);
+
+    pmt_string(const std::string& val);
+    pmt_string(const uint8_t* buf);
+    pmt_string(const pmtf::Pmt *fb_pmt);
+};
+
+
+} // namespace pmtf
---
+++ pmt/lib/pmtf_string.cpp
@@ -0,0 +1,46 @@
+#include <pmt/pmtf_string.hpp>
+#include <map>
+
+namespace pmtf {
+
+flatbuffers::Offset<void> pmt_string::rebuild_data(flatbuffers::FlatBufferBuilder& fbb)
+{
+    // fbb.Reset();
+    return CreatePmtStringDirect(fbb, value().c_str()).Union();
+}
+
+
+void pmt_string::set_value(const std::string& val)
+{
+    _data = CreatePmtStringDirect(_fbb, val.c_str()).Union();
+    build();
+}
+
+pmt_string::pmt_string(const std::string& val)
+    : pmt_base(Data::PmtString)
+{
+    set_value(val);
+}
+
+pmt_string::pmt_string(const uint8_t *buf)
+    : pmt_base(Data::PmtString)
+{
+    auto data = GetPmt(buf)->data_as_PmtString()->value();
+    set_value(*((const std::string*)data));
+}
+
+pmt_string::pmt_string(const pmtf::Pmt* fb_pmt)
+    : pmt_base(Data::PmtString)
+{
+    auto data = fb_pmt->data_as_PmtString()->value();
+    set_value(*((const std::string*)data));
+}
+
+std::string pmt_string::value()
+{
+    auto pmt = GetSizePrefixedPmt(_fbb.GetBufferPointer());
+    return std::string(pmt->data_as_PmtString()->value()->str());
+}
+
+
+} // namespace pmtf
