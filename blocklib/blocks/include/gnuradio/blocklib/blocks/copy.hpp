07a306a657fe8c08a2dd8f735a73c30dab135c3c
Garrett Vanhoy <gmvanhoy@gmail.com>
2021-03-04 12:56:54 -0500

    Simple comments

--- blocklib/blocks/include/gnuradio/blocklib/blocks/copy.hpp
+++ blocklib/blocks/include/gnuradio/blocklib/blocks/copy.hpp
@@ -7,6 +7,10 @@
 // in this code-base so that it can be easily included in an external library.
 // That implies that kernels cannot use the std::vector<block_work_input> and std::vector<block_work_output> 
 // as its function signature. It should be something like (int *in, int *out).
+
+// Kernels must have information about its type, device, and name (functionality) so that it 
+// can be matched with the correct block/port. It seems to me that the block class serves only the purpose
+// of glue between ports and kernels.
 namespace gr {
     namespace kernels {
         
