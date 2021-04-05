eac703e2dd4ad4ec86a8bbf4d504eb813e68916a
Josh Morman <jmorman@perspectalabs.com>
2021-04-05 13:52:36 -0400

    clean up copy template

--- blocklib/blocks/copy/copy_cpu.cpp
+++ blocklib/blocks/copy/copy_cpu.cpp
@@ -7,7 +7,7 @@ work_return_code_t copy_cpu::work(std::vector<block_work_input>& work_input,
                                   std::vector<block_work_output>& work_output)
 {
     auto* iptr = (uint8_t*)work_input[0].items();
-    int size = work_output[0].n_items * _itemsize;
+    int size = work_output[0].n_items * d_itemsize;
     auto* optr = (uint8_t*)work_output[0].items();
     std::copy(iptr, iptr + size, optr);
 
