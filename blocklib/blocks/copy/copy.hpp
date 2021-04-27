09b00d2b155951500de28465f104b3736cede246
Josh Morman <jmorman@perspectalabs.com>
2021-04-27 16:26:36 -0400

    blocks/copy: single make function and block args
    Signed-off-by: Josh Morman <jmorman@perspectalabs.com>

--- blocklib/blocks/copy/copy.hpp
+++ blocklib/blocks/copy/copy.hpp
@@ -6,11 +6,32 @@ namespace blocks {
 class copy : public sync_block
 {
 public:
+    typedef struct {
+        size_t itemsize;
+    } block_args;
+
+    enum class available_impl { CPU, CUDA };
+
     typedef std::shared_ptr<copy> sptr;
-    copy(size_t itemsize) : sync_block("copy")
+    copy(block_args args) : sync_block("copy")
     {
-        add_port(untyped_port::make("in", port_direction_t::INPUT, itemsize));
-        add_port(untyped_port::make("out", port_direction_t::OUTPUT, itemsize));
+        add_port(untyped_port::make("in", port_direction_t::INPUT, args.itemsize));
+        add_port(untyped_port::make("out", port_direction_t::OUTPUT, args.itemsize));
+    }
+
+    static sptr make(block_args args, available_impl impl = available_impl::CPU)
+    {
+        switch (impl) {
+        case available_impl::CPU:
+            make_cpu(args);
+            break;
+        // case available_impl::CUDA;
+        // make_cuda(args);
+        // break;
+        default:
+            throw std::invalid_argument(
+                "blocks::copy - invalid implementation specified");
+        }
     }
 
     /**
@@ -19,7 +40,7 @@ public:
      *
      * @return std::shared_ptr<copy>
      */
-    static sptr make_cpu(size_t itemsize);
+    static sptr make_cpu(block_args args);
 };
 
 } // namespace blocks
--- blocklib/blocks/copy/copy_cpu.hpp
+++ blocklib/blocks/copy/copy_cpu.hpp
@@ -8,7 +8,7 @@ namespace blocks {
 class copy_cpu : public copy
 {
 public:
-    copy_cpu(size_t itemsize) : copy(itemsize) {}
+    copy_cpu(block_args args) : copy(args), d_itemsize(args.itemsize) {}
     virtual work_return_code_t work(std::vector<block_work_input>& work_input,
                                     std::vector<block_work_output>& work_output) override;
 
