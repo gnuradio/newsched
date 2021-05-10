b1fe92e05d040cdc7b60a3039aecb64a4e55a751
Josh Morman <jmorman@perspectalabs.com>
2021-05-10 07:08:08 -0400

    blocks: cuda cleanup for multiply_const
    Signed-off-by: Josh Morman <jmorman@perspectalabs.com>

--- blocklib/blocks/multiply_const/multiply_const_cuda.cpp
+++ blocklib/blocks/multiply_const/multiply_const_cuda.cpp
@@ -1,25 +1,18 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2004,2009,2010,2012,2018 Free Software Foundation, Inc.
+ * Copyright 2021 Joshua Morman
  *
  * This file is part of GNU Radio
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
  */
-
+#include "multiply_const_cuda.cuh"
 #include "multiply_const_cuda.hpp"
 #include <volk/volk.h>
 
 namespace gr {
 namespace blocks {
-
-template <typename T>
-void exec_multiply_const_kernel(
-    const T* in, T* out, T k, int grid_size, int block_size, cudaStream_t stream);
-
-template <typename T>
-extern void get_block_and_grid_multiply_const(int* minGrid, int* minBlock);
 
 template <class T>
 typename multiply_const<T>::sptr multiply_const<T>::make_cuda(const block_args& args)
@@ -32,7 +25,19 @@ multiply_const_cuda<T>::multiply_const_cuda(
     const typename multiply_const<T>::block_args& args)
     : multiply_const<T>(args), d_k(args.k), d_vlen(args.vlen)
 {
-    get_block_and_grid_multiply_const<T>(&d_min_grid_size, &d_block_size);
+    multiply_const_cu::get_block_and_grid<T>(&d_min_grid_size, &d_block_size);
+    std::cout << "minGrid: " << d_min_grid_size << ", blockSize: " << d_block_size
+              << std::endl;
+
+    cudaStreamCreate(&d_stream);
+}
+
+template <>
+multiply_const_cuda<gr_complex>::multiply_const_cuda(
+    const typename multiply_const<gr_complex>::block_args& args)
+    : multiply_const<gr_complex>(args), d_k(args.k), d_vlen(args.vlen)
+{
+    multiply_const_cu::get_block_and_grid<cuFloatComplex>(&d_min_grid_size, &d_block_size);
     std::cout << "minGrid: " << d_min_grid_size << ", blockSize: " << d_block_size
               << std::endl;
 
@@ -51,7 +56,7 @@ multiply_const_cuda<T>::work(std::vector<block_work_input>& work_input,
 
     auto noutput_items = work_output[0].n_items;
 
-    exec_multiply_const_kernel(
+    multiply_const_cu::exec_kernel(
         in, out, d_k, (noutput_items * d_vlen) / d_block_size, d_block_size, d_stream);
 
     work_output[0].n_produced = work_output[0].n_items;
@@ -59,10 +64,31 @@ multiply_const_cuda<T>::work(std::vector<block_work_input>& work_input,
     return work_return_code_t::WORK_OK;
 }
 
-// template class multiply_const<std::int16_t>;
-// template class multiply_const<std::int32_t>;
+template <>
+work_return_code_t
+multiply_const_cuda<gr_complex>::work(std::vector<block_work_input>& work_input,
+                             std::vector<block_work_output>& work_output)
+{
+    // Pre-generate these from modtool, for example
+    auto in = static_cast<cuFloatComplex*>(work_input[0].items());
+    auto out = static_cast<cuFloatComplex*>(work_output[0].items());
+
+    auto noutput_items = work_output[0].n_items;
+
+    auto k_cufc = make_cuFloatComplex(real(d_k), imag(d_k));
+
+    multiply_const_cu::exec_kernel(
+        in, out, k_cufc, (noutput_items * d_vlen) / d_block_size, d_block_size, d_stream);
+
+    work_output[0].n_produced = work_output[0].n_items;
+    work_input[0].n_consumed = work_input[0].n_items;
+    return work_return_code_t::WORK_OK;
+}
+
+template class multiply_const<std::int16_t>;
+template class multiply_const<std::int32_t>;
 template class multiply_const<float>;
-// template class multiply_const<gr_complex>;
+template class multiply_const<gr_complex>;
 
 } /* namespace blocks */
 } /* namespace gr */
--- blocklib/blocks/multiply_const/multiply_const_cuda.cu
+++ blocklib/blocks/multiply_const/multiply_const_cuda.cu
@@ -1,11 +1,13 @@
+#include "multiply_const_cuda.cuh"
 #include <cuComplex.h>
+#include <complex>
 
 namespace gr {
 namespace blocks {
+namespace multiply_const_cu {
 
 template <typename T>
-__global__ 
-void multiply_const_kernel(const T* in, T* out, T k, size_t n)
+__global__ void multiply_const_kernel(const T* in, T* out, T k, size_t n)
 {
     auto i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < n)
@@ -13,34 +15,59 @@ void multiply_const_kernel(const T* in, T* out, T k, size_t n)
 }
 
 template <>
-__global__ 
-void multiply_const_kernel(const cuFloatComplex* in, cuFloatComplex* out, cuFloatComplex k, size_t n)
+__global__ void multiply_const_kernel(const cuFloatComplex* in,
+                                      cuFloatComplex* out,
+                                      cuFloatComplex k,
+                                      size_t n)
 {
     auto i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n)
-    {
-        out[i].x = k.x * in[i].x - k.y*in[i].y;
-        out[i].y = k.x * in[i].y + k.y*in[i].x;
+    if (i < n) {
+        out[i].x = k.x * in[i].x - k.y * in[i].y;
+        out[i].y = k.x * in[i].y + k.y * in[i].x;
     }
 }
 
 template <typename T>
-void exec_multiply_const_kernel(
+void exec_kernel(
     const T* in, T* out, T k, int grid_size, int block_size, cudaStream_t stream)
 {
-    multiply_const_kernel<T><<<grid_size, block_size, 0, stream>>>(in, out, k, block_size * grid_size);
-} 
+    multiply_const_kernel<T>
+        <<<grid_size, block_size, 0, stream>>>(in, out, k, block_size * grid_size);
+}
+
+// template <>
+// void exec_kernel(const std::complex<float>* in,
+//                  std::complex<float>* out,
+//                  std::complex<float> k,
+//                  int grid_size,
+//                  int block_size,
+//                  cudaStream_t stream)
+// {
+//     multiply_const_kernel<cuFloatComplex>
+//         <<<grid_size, block_size, 0, stream>>>(reinterpret_cast<const cuFloatComplex*>(in),
+//                                                reinterpret_cast<cuFloatComplex*>(out),
+//                                                make_cuFloatComplex(real(k), imag(k)),
+//                                                block_size * grid_size);
+// }
 
 template <typename T>
-void get_block_and_grid_multiply_const(int* minGrid, int* minBlock)
+void get_block_and_grid(int* minGrid, int* minBlock)
 {
     // https://developer.nvidia.com/blog/cuda-pro-tip-occupancy-api-simplifies-launch-configuration/
     cudaOccupancyMaxPotentialBlockSize(minGrid, minBlock, multiply_const_kernel<T>, 0, 0);
 }
 
-template void exec_multiply_const_kernel<float>(
-    const float* in, float* out, float k, int grid_size, int block_size, cudaStream_t stream);
-template void get_block_and_grid_multiply_const<float>(int* minGrid, int* minBlock);
+#define IMPLEMENT_KERNEL(T)                                                            \
+    template void exec_kernel<T>(                                                      \
+        const T* in, T* out, T k, int grid_size, int block_size, cudaStream_t stream); \
+    template void get_block_and_grid<T>(int* minGrid, int* minBlock);
 
+
+IMPLEMENT_KERNEL(int16_t)
+IMPLEMENT_KERNEL(int32_t)
+IMPLEMENT_KERNEL(float)
+IMPLEMENT_KERNEL(cuFloatComplex)
+
+} // namespace multiply_const_cu
 } // namespace blocks
 } // namespace gr
