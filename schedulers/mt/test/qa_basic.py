5ae8068fa094f4d8736e0fee17b968799181f77a
Josh Morman <jmorman@perspectalabs.com>
2021-05-21 19:36:28 -0400

    add qa test as gr_unittest

--- schedulers/mt/test/qa_basic.py
+++ schedulers/mt/test/qa_basic.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python3
 
-from newsched import gr, blocks
+from newsched import gr_unittest, gr, blocks
 from newsched.schedulers import mt
 
+class test_basic(gr_unittest.TestCase):
 
-def main():
-    nsamples = 100000
-    input_data = list(range(nsamples))
+    def setUp(self):
+        self.tb = gr.flowgraph()
 
-    src = blocks.vector_source_f(input_data, False)
-    cp1 = blocks.copy(gr.sizeof_float)
-    cp2 = blocks.copy(gr.sizeof_float)
-    snk1 = blocks.vector_sink_f()
-    snk2 = blocks.vector_sink_f()
+    def tearDown(self):
+        self.tb = None
 
-    fg = gr.flowgraph()
-    fg.connect(src, 0, cp1, 0)
-    fg.connect(cp1, 0, snk1, 0)
-    fg.connect(src, 0, cp2, 0)
-    fg.connect(cp2, 0, snk2, 0)
+    def test_basic(self):
+        nsamples = 100000
+        input_data = list(range(nsamples))
 
-    sched = mt.scheduler_mt()
-    fg.set_scheduler(sched)
+        src = blocks.vector_source_f(input_data, False)
+        cp1 = blocks.copy(gr.sizeof_float)
+        cp2 = blocks.copy(gr.sizeof_float)
+        snk1 = blocks.vector_sink_f()
+        snk2 = blocks.vector_sink_f()
 
-    fg.validate()
+        fg = gr.flowgraph()
+        fg.connect(src, 0, cp1, 0)
+        fg.connect(cp1, 0, snk1, 0)
+        fg.connect(src, 0, cp2, 0)
+        fg.connect(cp2, 0, snk2, 0)
 
-    fg.start()
-    fg.wait()
+        sched = mt.scheduler_mt()
+        fg.set_scheduler(sched)
 
-    print(snk1.data() == input_data)
-    print(snk2.data() == input_data)
+        fg.validate()
 
+        fg.start()
+        fg.wait()
+
+        self.assertEqual(input_data, snk1.data())
+        self.assertEqual(input_data, snk2.data())
 
 if __name__ == "__main__":
-    main()
+    gr_unittest.run(test_basic)
