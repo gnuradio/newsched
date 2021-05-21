ee742c56d3924b733837930af6de5d8ff589bf7d
Josh Morman <jmorman@perspectalabs.com>
2021-05-21 11:37:38 -0400

    some basic python bindings

---
+++ blocklib/blocks/copy/copy_pybind.cc
@@ -0,0 +1,27 @@
+#include <pybind11/complex.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+#include <gnuradio/blocks/copy.hh>
+// pydoc.h is automatically generated in the build directory
+// #include <copy_pydoc.h>
+
+void bind_copy(py::module& m)
+{
+    using copy = ::gr::blocks::copy;
+
+    py::class_<copy, std::shared_ptr<copy>> copy_class(m, "copy");
+
+    py::enum_<::gr::blocks::copy::available_impl>(copy_class, "available_impl")
+        .value("cpu", ::gr::blocks::copy::available_impl::CPU)   // 0
+        .value("cuda", ::gr::blocks::copy::available_impl::CUDA) // 1
+        .export_values();
+
+    copy_class.def(py::init([](size_t itemsize, gr::blocks::copy::available_impl impl) {
+                       return copy::make({ itemsize }, impl);
+                   }),
+                   py::arg("itemsize"),
+                   py::arg("impl") = gr::blocks::copy::available_impl::CPU);
+}
