f6a4c1f9c301878efd1b868b69c8849eed041cb5
Josh Morman <jmorman@perspectalabs.com>
2021-05-24 15:53:42 -0400

    qa/bench: updates related to restructuring blocks
    Signed-off-by: Josh Morman <jmorman@perspectalabs.com>

--- schedulers/mt/test/cuda/qa_scheduler_mt_cuda_copy.cc
+++ schedulers/mt/test/cuda/qa_scheduler_mt_cuda_copy.cc
@@ -6,8 +6,10 @@
 
 #include <gnuradio/blocks/vector_sink.hh>
 #include <gnuradio/blocks/vector_source.hh>
-#include <gnuradio/cuda/copy.hh>
+// #include <gnuradio/cuda/copy.hh>
+#include <gnuradio/blocks/copy.hh>
 #include <gnuradio/cudabuffer.hh>
+#include <gnuradio/cudabuffer_pinned.hh>
 #include <gnuradio/flowgraph.hh>
 #include <gnuradio/schedulers/mt/scheduler_mt.hh>
 
@@ -19,7 +21,7 @@ using namespace gr;
  */
 TEST(SchedulerMTTest, CudaCopyBasic)
 {
-    int veclen = 1024;
+    size_t veclen = 1024;
     int num_samples = veclen*100;
     std::vector<gr_complex> input_data(num_samples);
 
@@ -27,10 +29,10 @@ TEST(SchedulerMTTest, CudaCopyBasic)
         input_data[i] = gr_complex(i, -i);
     }
 
-    auto src = blocks::vector_source_c::make(input_data, false, veclen);
-    auto snk1 = blocks::vector_sink_c::make(veclen);
-    auto copy1 = cuda::copy::make(veclen);
-    auto copy2 = cuda::copy::make(veclen);
+    auto src = blocks::vector_source_c::make_cpu({input_data, false, veclen});
+    auto snk1 = blocks::vector_sink_c::make_cpu({veclen});
+    auto copy1 = blocks::copy::make_cuda({veclen*sizeof(gr_complex)});
+    auto copy2 = blocks::copy::make_cuda({veclen*sizeof(gr_complex)});
 
     auto fg = flowgraph::make();
     fg->connect(src, 0, copy1, 0)->set_custom_buffer(CUDA_BUFFER_ARGS_H2D);
@@ -56,23 +58,26 @@ TEST(SchedulerMTTest, CudaCopyBasic)
  */
 TEST(SchedulerMTTest, CudaCopyMultiThreaded)
 {
-    int veclen = 1024;
-    int num_samples = veclen*100;
+    size_t veclen = 1024;
+    int num_samples = veclen*1000;
     std::vector<gr_complex> input_data(num_samples);
 
     for (int i = 0; i < num_samples; i++) {
         input_data[i] = gr_complex(i, -i);
     }
 
-    auto src = blocks::vector_source_c::make(input_data, false, veclen);
-    auto snk1 = blocks::vector_sink_c::make(veclen);
-    auto copy1 = cuda::copy::make(veclen);
-    auto copy2 = cuda::copy::make(veclen);
+    auto src = blocks::vector_source_c::make_cpu({input_data, false, veclen});
+    auto snk1 = blocks::vector_sink_c::make_cpu({veclen});
+    auto copy1 = blocks::copy::make_cuda({veclen*sizeof(gr_complex)});
+    auto copy2 = blocks::copy::make_cuda({veclen*sizeof(gr_complex)});
 
     auto fg = flowgraph::make();
-    fg->connect(src, 0, copy1, 0)->set_custom_buffer(CUDA_BUFFER_ARGS_H2D);
-    fg->connect(copy1, 0, copy2, 0)->set_custom_buffer(CUDA_BUFFER_ARGS_D2D);
-    fg->connect(copy2, 0, snk1, 0)->set_custom_buffer(CUDA_BUFFER_ARGS_D2H);
+    // fg->connect(src, 0, copy1, 0)->set_custom_buffer(CUDA_BUFFER_ARGS_H2D);
+    // fg->connect(copy1, 0, copy2, 0)->set_custom_buffer(CUDA_BUFFER_ARGS_D2D);
+    // fg->connect(copy2, 0, snk1, 0)->set_custom_buffer(CUDA_BUFFER_ARGS_D2H);
+    fg->connect(src, 0, copy1, 0)->set_custom_buffer(CUDA_BUFFER_PINNED_ARGS);
+    fg->connect(copy1, 0, copy2, 0)->set_custom_buffer(CUDA_BUFFER_PINNED_ARGS);
+    fg->connect(copy2, 0, snk1, 0)->set_custom_buffer(CUDA_BUFFER_PINNED_ARGS);
 
     auto sched = schedulers::scheduler_mt::make("sched", 32768);
     fg->set_scheduler(sched);
@@ -83,7 +88,15 @@ TEST(SchedulerMTTest, CudaCopyMultiThreaded)
     fg->start();
     fg->wait();
 
-    EXPECT_EQ(snk1->data().size(), input_data.size());
-    EXPECT_EQ(snk1->data(), input_data);
+    auto outdata = snk1->data();
+
+    EXPECT_EQ(outdata.size(), input_data.size());
+    EXPECT_EQ(outdata, input_data);
+
+    // for (int i=0; i<outdata.size(); i++)
+    // {
+    //     if (outdata[i] != input_data[i])
+    //     std::cout << outdata[i] << " " << input_data[i] << std::endl;
+    // }
     
 }
