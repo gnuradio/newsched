8fbcbc83b4e649b4cb337d1cec16abd8c9e15d67
Josh Morman <jmorman@perspectalabs.com>
2021-05-26 14:40:42 -0400

    blocks: pybind the nop blocks
    Signed-off-by: Josh Morman <jmorman@perspectalabs.com>

---
+++ blocklib/blocks/nop_source/nop_source_cpu.hh
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <gnuradio/blocks/nop_source.hh>
+
+namespace gr {
+namespace blocks {
+
+class nop_source_cpu : public nop_source
+{
+public:
+    nop_source_cpu(block_args args) : nop_source(args), d_itemsize(args.itemsize) {}
+    virtual work_return_code_t work(std::vector<block_work_input>& work_input,
+                                    std::vector<block_work_output>& work_output) override;
+
+protected:
+    size_t d_itemsize;
+    size_t d_nports;
+};
+
+} // namespace blocks
+} // namespace gr
