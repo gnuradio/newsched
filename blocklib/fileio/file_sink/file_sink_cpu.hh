9bdb227663359d76dab135db3d57f31c5ac7c5cf
Josh Morman <jmorman@perspectalabs.com>
2021-06-16 09:16:14 -0400

    fileio: add file_sink
    Signed-off-by: Josh Morman <jmorman@perspectalabs.com>

---
+++ blocklib/fileio/file_sink/file_sink_cpu.hh
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <gnuradio/fileio/file_sink.hh>
+#include <gnuradio/fileio/file_sink_base.hh>
+
+namespace gr {
+namespace fileio {
+
+class file_sink_cpu : public file_sink, virtual public file_sink_base
+{
+public:
+    file_sink_cpu(const block_args& args);
+    ~file_sink_cpu() override;
+
+    bool stop() override;
+
+    virtual work_return_code_t work(std::vector<block_work_input>& work_input,
+                                    std::vector<block_work_output>& work_output) override;
+
+private:
+    size_t d_itemsize;
+};
+
+} // namespace fileio
+} // namespace gr
