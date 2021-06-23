3d8cc937016e953c7ca3d2f2f8f1a08329316e94
Josh Morman <jmorman@perspectalabs.com>
2021-06-23 10:29:18 -0400

    math: remove output_multiple from complex_to_
    Signed-off-by: Josh Morman <jmorman@perspectalabs.com>

--- blocklib/math/complex_to_mag/complex_to_mag_cpu.hh
+++ blocklib/math/complex_to_mag/complex_to_mag_cpu.hh
@@ -10,8 +10,8 @@ class complex_to_mag_cpu : public complex_to_mag
 public:
     complex_to_mag_cpu(const block_args& args) : complex_to_mag(args), d_vlen(args.vlen)
     {
-        const int alignment_multiple = volk_get_alignment() / sizeof(float);
-        set_output_multiple(std::max(1, alignment_multiple));
+        // const int alignment_multiple = volk_get_alignment() / sizeof(float);
+        // set_output_multiple(std::max(1, alignment_multiple));
     }
     virtual work_return_code_t work(std::vector<block_work_input>& work_input,
                                     std::vector<block_work_output>& work_output) override;
