3ff62aec350703c16e74faab09a42b520bfe3446
Josh Morman <jmorman@perspectalabs.com>
2021-06-23 09:21:44 -0400

    math: use output_multiple for volk alignment
    Signed-off-by: Josh Morman <jmorman@perspectalabs.com>

--- blocklib/math/complex_to_mag_squared/complex_to_mag_squared_cpu.hh
+++ blocklib/math/complex_to_mag_squared/complex_to_mag_squared_cpu.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <gnuradio/math/complex_to_mag_squared.hh>
+#include <volk/volk.h>
 
 namespace gr {
 namespace math {
@@ -8,13 +9,17 @@ namespace math {
 class complex_to_mag_squared_cpu : public complex_to_mag_squared
 {
 public:
-    complex_to_mag_squared_cpu(const block_args& args) : complex_to_mag_squared(args), d_vlen(args.vlen) {}
+    complex_to_mag_squared_cpu(const block_args& args)
+        : complex_to_mag_squared(args), d_vlen(args.vlen)
+    {
+        const int alignment_multiple = volk_get_alignment() / sizeof(float);
+        set_output_multiple(std::max(1, alignment_multiple));
+    }
     virtual work_return_code_t work(std::vector<block_work_input>& work_input,
                                     std::vector<block_work_output>& work_output) override;
 
 private:
     size_t d_vlen;
-
 };
 
 } // namespace math
