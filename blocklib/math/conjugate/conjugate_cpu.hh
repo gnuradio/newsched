e9a3d053e3f2b01ed4b979f133b5bb549f2db227
Josh Morman <jmorman@peratonlabs.com>
2021-08-30 15:24:55 -0400

    math: add blocks and cuda implementations
    Signed-off-by: Josh Morman <jmorman@peratonlabs.com>

--- blocklib/math/conjugate/conjugate_cpu.hh
+++ blocklib/math/conjugate/conjugate_cpu.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <gnuradio/math/conjugate.hh>
+#include <volk/volk.h>
 
 namespace gr {
 namespace math {
@@ -8,10 +9,13 @@ namespace math {
 class conjugate_cpu : public conjugate
 {
 public:
-    conjugate_cpu(const block_args& args) : conjugate(args) {}
+    conjugate_cpu(const block_args& args) : conjugate(args)
+    {
+        // const int alignment_multiple = volk_get_alignment() / sizeof(gr_complex);
+        // set_output_multiple(std::max(1, alignment_multiple));
+    }
     virtual work_return_code_t work(std::vector<block_work_input>& work_input,
                                     std::vector<block_work_output>& work_output) override;
-
 };
 
 } // namespace math
