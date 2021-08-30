a40dbcdd500dab51d27073945d05f45004a898fc
Josh Morman <jmorman@peratonlabs.com>
2021-08-30 12:08:37 -0400

    filter: add moving_average with CUDA impl
    Signed-off-by: Josh Morman <jmorman@peratonlabs.com>

---
+++ blocklib/filter/moving_average/moving_average_cuda.hh
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <gnuradio/filter/moving_average.hh>
+
+#include <vector>
+
+#include <cusp/convolve.cuh>
+
+namespace gr {
+namespace filter {
+
+template <class T>
+class moving_average_cuda : public moving_average<T>
+{
+public:
+    moving_average_cuda(const typename moving_average<T>::block_args& args);
+    
+    virtual work_return_code_t work(std::vector<block_work_input>& work_input,
+                                    std::vector<block_work_output>& work_output) override;
+
+    int group_delay();
+
+protected:
+    size_t d_length;
+    T d_scale;
+    size_t d_max_iter;
+    size_t d_vlen;
+
+    size_t d_new_length;
+    T d_new_scale;
+    bool d_updated = false;
+
+    std::shared_ptr<cusp::convolve<T,T>> p_kernel_full;
+    std::shared_ptr<cusp::convolve<T,T>> p_kernel_valid;
+
+};
+
+
+} // namespace filter
+} // namespace gr
