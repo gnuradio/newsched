66b3102d39cfcfa9491e7aeec62513d9a574fc43
Josh Morman <jmorman@peratonlabs.com>
2021-08-30 12:08:37 -0400

    filter: add moving_average with CUDA impl
    Signed-off-by: Josh Morman <jmorman@peratonlabs.com>

--- blocklib/filter/moving_average/moving_average_cpu.cc
+++ blocklib/filter/moving_average/moving_average_cpu.cc
@@ -41,50 +41,61 @@ work_return_code_t
 moving_average_cpu<T>::work(std::vector<block_work_input>& work_input,
                             std::vector<block_work_output>& work_output)
 {
+    if (work_input[0].n_items < (int) d_length)
+    {
+        work_output[0].n_produced = 0;
+        work_input[0].n_consumed = 0;
+        return work_return_code_t::WORK_INSUFFICIENT_INPUT_ITEMS;
+    }
+
     if (d_updated) {
         d_length = d_new_length;
         d_scale = d_new_scale;
         d_updated = false;
         work_output[0].n_produced = 0;
+        work_input[0].n_consumed = 0;
         return work_return_code_t::WORK_OK;
     }
 
     auto in = static_cast<const T*>(work_input[0].items());
     auto out = static_cast<T*>(work_output[0].items());
 
-    size_t noutput_items = std::min(work_input[0].n_items, work_output[0].n_items);
+    size_t noutput_items = std::min( (int) ( work_input[0].n_items - d_length ), work_output[0].n_items);
 
     auto num_iter = (noutput_items > d_max_iter) ? d_max_iter : noutput_items;
+    auto tr = work_input[0].buffer->total_read();
 
+    if (tr == 0) { // for the first no history case
+        for (size_t i = 0; i < num_iter; i++) {
+            for (size_t elem = 0; elem < d_vlen; elem++) {
+                d_sum[elem] += in[i * d_vlen + elem];
+                out[i * d_vlen + elem] = d_sum[elem] * d_scale;
+                if (i >= (d_length-1)) {
+                    d_sum[elem] -= in[(i - (d_length - 1)) * d_vlen + elem];
+                }
+            }
+        }
+    } else {
+        for (size_t i = 0; i < num_iter; i++) {
+            for (size_t elem = 0; elem < d_vlen; elem++) {
 
-    for (size_t i = 0; i < num_iter; i++) {
-        for (size_t elem = 0; elem < d_vlen; elem++) {
-            d_sum[elem] += in[i * d_vlen + elem];
-            out[i * d_vlen + elem] = d_sum[elem] * d_scale;
-            if (i >= (d_length - 1)) {
-                d_sum[elem] -= in[(i - (d_length - 1)) * d_vlen + elem];
-            } else {
-                d_sum[elem] -= d_history[i * d_vlen + elem];
+                d_sum[elem] += in[(i+d_length-1) * d_vlen + elem];
+                out[i * d_vlen + elem] = d_sum[elem] * d_scale;
+                d_sum[elem] -= in[i * d_vlen + elem];
             }
         }
     }
 
-    // Stash the history (since GR 4.0 no longer has block history)
-    // TODO: don't consume all the inputs, and don't copy here
-    //  -- but still need to handle the first buffer that has no unused inputs
-    memcpy(
-        d_history.data(), &in[num_iter - d_length + 1], d_vlen * sizeof(T) * (d_length - 1));
-
-
+    // don't consume the last d_length-1 samples
     work_output[0].n_produced = num_iter;
-    work_input[0].n_consumed = num_iter;
+    work_input[0].n_consumed = tr == 0 ? num_iter - (d_length - 1) : num_iter;
     return work_return_code_t::WORK_OK;
-}
+} // namespace filter
 
-template class moving_average<int16_t>;
-template class moving_average<int32_t>;
+// template class moving_average<int16_t>;
+// template class moving_average<int32_t>;
 template class moving_average<float>;
 template class moving_average<gr_complex>;
 
-} /* namespace filter */
+} // namespace filter
 } /* namespace gr */
---
+++ blocklib/filter/moving_average/moving_average_cuda.cc
@@ -0,0 +1,85 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2004,2009,2010,2012,2018 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
+#include "moving_average_cuda.hh"
+
+namespace gr {
+namespace filter {
+
+template <class T>
+typename moving_average<T>::sptr
+moving_average<T>::make_cuda(const moving_average<T>::block_args& args)
+{
+    return std::make_shared<moving_average_cuda<T>>(args);
+}
+
+template <class T>
+moving_average_cuda<T>::moving_average_cuda(
+    const typename moving_average<T>::block_args& args)
+    : moving_average<T>(args),
+      d_length(args.length),
+      d_scale(args.scale),
+      d_max_iter(args.max_iter),
+      d_vlen(args.vlen),
+      d_new_length(args.length),
+      d_new_scale(args.scale)
+{
+    std::vector<T> taps(d_length);
+    for (size_t i = 0; i < d_length; i++) {
+        taps[i] = (float)1.0 * d_scale;
+    }
+
+    p_kernel_full =
+        std::make_shared<cusp::convolve<T, T>>(taps, cusp::convolve_mode_t::FULL_TRUNC);
+    p_kernel_valid =
+        std::make_shared<cusp::convolve<T, T>>(taps, cusp::convolve_mode_t::VALID);
+}
+
+template <class T>
+work_return_code_t
+moving_average_cuda<T>::work(std::vector<block_work_input>& work_input,
+                             std::vector<block_work_output>& work_output)
+{
+    if (work_input[0].n_items < (int)d_length) {
+        work_output[0].n_produced = 0;
+        work_input[0].n_consumed = 0;
+        return work_return_code_t::WORK_INSUFFICIENT_INPUT_ITEMS;
+    }
+
+    auto in = static_cast<const T*>(work_input[0].items());
+    auto out = static_cast<T*>(work_output[0].items());
+
+    size_t noutput_items =
+        std::min((int)(work_input[0].n_items), work_output[0].n_items);
+
+    // auto num_iter = (noutput_items > d_max_iter) ? d_max_iter : noutput_items;
+    auto num_iter = noutput_items;
+    auto tr = work_input[0].buffer->total_read();
+
+    if (tr == 0) {
+        p_kernel_full->launch_default_occupancy({ in }, { out }, num_iter);
+    } else {
+        p_kernel_valid->launch_default_occupancy({ in }, { out }, num_iter);
+    }
+
+    // don't consume the last d_length-1 samples
+    work_output[0].n_produced = tr == 0 ? num_iter : num_iter - (d_length - 1);
+    work_input[0].n_consumed =
+        tr == 0 ? num_iter - (d_length - 1) : num_iter - (d_length - 1);
+    return work_return_code_t::WORK_OK;
+} // namespace filter
+
+// template class moving_average<int16_t>;
+// template class moving_average<int32_t>;
+template class moving_average<float>;
+template class moving_average<gr_complex>;
+
+} // namespace filter
+} /* namespace gr */
