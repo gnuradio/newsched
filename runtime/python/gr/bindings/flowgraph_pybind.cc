cf72ab2a78ad58a17df4accc22fb847afde5c0c0
Josh Morman <jmorman@peratonlabs.com>
2021-10-13 16:32:44 -0400

    runtime: ctrl-c handling in python bindings
    Signed-off-by: Josh Morman <jmorman@peratonlabs.com>

--- runtime/python/gr/bindings/flowgraph_pybind.cc
+++ runtime/python/gr/bindings/flowgraph_pybind.cc
@@ -11,6 +11,7 @@
 #include <pybind11/complex.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <thread>
 
 namespace py = pybind11;
 
@@ -20,7 +21,8 @@ namespace py = pybind11;
 
 void bind_flowgraph(py::module& m)
 {
-    py::class_<gr::flowgraph, gr::graph, gr::node, std::shared_ptr<gr::flowgraph>>(m, "flowgraph")
+    py::class_<gr::flowgraph, gr::graph, gr::node, std::shared_ptr<gr::flowgraph>>(
+        m, "flowgraph")
 
         .def(py::init(&gr::flowgraph::make))
         .def("set_scheduler", &gr::flowgraph::set_scheduler, py::arg("sched"))
@@ -31,6 +33,29 @@ void bind_flowgraph(py::module& m)
         .def("validate", &gr::flowgraph::validate)
         .def("start", &gr::flowgraph::start)
         .def("stop", &gr::flowgraph::stop)
-        .def("wait", &gr::flowgraph::wait)
-        .def("run", &gr::flowgraph::run);
+        .def("wait",
+             [](gr::flowgraph& self) {
+                 std::thread th([] {
+                     for (;;) {
+                         if (PyErr_CheckSignals() != 0)
+                             throw py::error_already_set();
+                         std::this_thread::sleep_for(
+                             std::chrono::milliseconds(100));
+                     }
+                 });
+                 th.detach();
+                 self.wait();
+             })
+        .def("run", [](gr::flowgraph& self) {
+            std::thread th([] {
+                for (;;) {
+                    if (PyErr_CheckSignals() != 0)
+                        throw py::error_already_set();
+                    std::this_thread::sleep_for(
+                        std::chrono::milliseconds(100));
+                }
+            });
+            th.detach();
+            self.run();
+        });
 }
