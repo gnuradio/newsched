fdec5d209909f56c398c6417d274412aca8f1083
Josh Morman <jmorman@peratonlabs.com>
2021-10-18 13:12:53 -0400

    runtime: additional bindings for buffer settings
    Signed-off-by: Josh Morman <jmorman@peratonlabs.com>

--- runtime/python/gr/bindings/buffer_pybind.cc
+++ runtime/python/gr/bindings/buffer_pybind.cc
@@ -21,7 +21,13 @@ void bind_buffer(py::module& m)
 {
     using buffer_properties = ::gr::buffer_properties;
 
-    py::class_<buffer_properties, std::shared_ptr<buffer_properties>>(
-        m, "buffer_properties")
-        .def("set_buffer_size", &buffer_properties::set_buffer_size);
+    py::class_<buffer_properties, std::shared_ptr<buffer_properties>>(m,
+                                                                      "buffer_properties")
+        .def("set_buffer_size", &buffer_properties::set_buffer_size)
+        .def("set_max_buffer_size", &buffer_properties::set_max_buffer_size)
+        .def("set_min_buffer_size", &buffer_properties::set_min_buffer_size)
+        .def("set_max_buffer_fill", &buffer_properties::set_max_buffer_fill)
+        .def("set_min_buffer_fill", &buffer_properties::set_min_buffer_fill)
+        .def("set_max_buffer_read", &buffer_properties::set_max_buffer_read)
+        .def("set_min_buffer_read", &buffer_properties::set_min_buffer_read);
 }
