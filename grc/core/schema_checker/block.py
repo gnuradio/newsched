cfa9fa9227c10a2ad6604094a473ea4edb931df7
Josh Morman <jmorman@gnuradio.org>
2021-10-22 11:47:48 -0400

    grc: add core dir (skipped by git)
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

---
+++ grc/core/schema_checker/block.py
@@ -0,0 +1,135 @@
+from .utils import Spec, expand
+
+PARAM_SCHEME = expand(
+    base_key=str,   # todo: rename/remove
+
+    id=str,
+    label=str,
+    category=str,
+
+    dtype=str,
+    default=object,
+
+    options=list,
+    option_labels=list,
+    option_attributes=Spec(types=dict, required=False, item_scheme=(str, list)),
+
+    hide=str,
+)
+PORT_SCHEME = expand(
+    label=str,
+    domain=str,
+
+    id=str,
+    dtype=str,
+    vlen=(int, str),
+
+    multiplicity=(int, str),
+    optional=(bool, int, str),
+    hide=(bool, str),
+)
+TEMPLATES_SCHEME = expand(
+    imports=str,
+    var_make=str,
+    var_value=str,
+    make=str,
+    callbacks=list,
+)
+CPP_TEMPLATES_SCHEME = expand(
+    includes=list,
+    declarations=str,
+    make=str,
+    var_make=str,
+    callbacks=list,
+    link=list,
+    packages=list,
+    translations=dict,
+)
+BLOCK_SCHEME = expand(
+    id=Spec(types=str, required=True, item_scheme=None),
+    label=str,
+    category=str,
+    flags=(list, str),
+
+    parameters=Spec(types=list, required=False, item_scheme=PARAM_SCHEME),
+    inputs=Spec(types=list, required=False, item_scheme=PORT_SCHEME),
+    outputs=Spec(types=list, required=False, item_scheme=PORT_SCHEME),
+
+    asserts=(list, str),
+    value=str,
+
+    templates=Spec(types=dict, required=False, item_scheme=TEMPLATES_SCHEME),
+    cpp_templates=Spec(types=dict, required=False, item_scheme=CPP_TEMPLATES_SCHEME),
+
+    documentation=str,
+    grc_source=str,
+
+    file_format=Spec(types=int, required=True, item_scheme=None),
+
+    block_wrapper_path=str,  # todo: rename/remove
+)
+NEWSCHED_PROPERTIES_SCHEME = expand(
+    id=str,
+    label=str,
+    category=str,
+    value=str,
+)
+
+NEWSCHED_PARAM_SCHEME = expand(
+    id=str,
+    label=str,
+    dtype=str,
+    default=object,
+    settable=(str,bool),
+    gettable=(str,bool),
+
+    options=list,
+    option_labels=list,
+    option_attributes=Spec(types=dict, required=False, item_scheme=(str, list)),
+
+    hide=str,
+)
+NEWSCHED_PORT_SCHEME = expand(
+    label=str,
+    domain=str,
+    direction=str,
+    id=str,
+    type=str,
+    dims=str,
+
+
+    multiplicity=(int, str),
+    optional=(bool, int, str),
+    hide=(bool, str),
+)
+NEWSCHED_IMPL_SCHEME = expand(
+    id=str
+)
+
+NEWSCHED_BLOCK_SCHEME = expand(
+    block=Spec(types=str, required=True, item_scheme=None),
+    module=Spec(types=str, required=True, item_scheme=None),
+    label=str,
+    category=str,
+    flags=(list, str),
+
+    parameters=Spec(types=list, required=False, item_scheme=NEWSCHED_PARAM_SCHEME),
+    properties=Spec(types=list, required=False, item_scheme=NEWSCHED_PROPERTIES_SCHEME),
+    ports=Spec(types=list, required=False, item_scheme=NEWSCHED_PORT_SCHEME),
+
+    asserts=(list, str),
+    value=str,
+
+    templates=Spec(types=dict, required=False, item_scheme=TEMPLATES_SCHEME),
+    cpp_templates=Spec(types=dict, required=False, item_scheme=CPP_TEMPLATES_SCHEME),
+
+    documentation=str,
+    grc_source=str,
+
+    implementations=Spec(types=list, required=True, item_scheme=NEWSCHED_IMPL_SCHEME),
+
+    file_format=Spec(types=int, required=True, item_scheme=None),
+
+    block_wrapper_path=str,  # todo: rename/remove
+)
+
