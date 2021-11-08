2c9c1284dc1c98e37c51173deb00eebc9f74a0ac
Josh Morman <jmorman@gnuradio.org>
2021-11-08 09:16:23 -0500

    runtime: destructor for clonable_block interface
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- runtime/include/gnuradio/clonable_block.hh
+++ runtime/include/gnuradio/clonable_block.hh
@@ -9,10 +9,10 @@ namespace gr {
  * @brief clonable interface to block
  *
  */
-class clonable_block 
+class clonable_block
 {
 public:
+    virtual ~clonable_block() {}
     virtual std::shared_ptr<block> clone() const = 0;
-
 };
 } // namespace gr
