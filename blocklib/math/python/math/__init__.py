f3ddafa06a7259f015c1cf9674d37400548fb852
Josh Morman <jmorman@gnuradio.org>
2021-12-08 07:19:39 -0500

    blocklib: reconfigure the file loc for pure python impl
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- blocklib/math/python/math/__init__.py
+++ blocklib/math/python/math/__init__.py
@@ -9,4 +9,5 @@ except ImportError:
     __path__.append(os.path.join(dirname, "bindings"))
     from .math_python import *
 
-sys.path.extend([f'./{name}' for name in os.listdir(".") if os.path.isdir(name)])
+# sys.path.extend([f'./{name}' for name in os.listdir(".") if os.path.isdir(name)])
+from . import numpy
