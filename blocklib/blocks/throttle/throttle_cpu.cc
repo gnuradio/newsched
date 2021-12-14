ff05f2e0537a5447e0c59dec602fb68a93570cde
Josh Morman <jmorman@gnuradio.org>
2021-12-14 16:25:07 -0500

    blocks: use notify message for throttle
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- blocklib/blocks/throttle/throttle_cpu.cc
+++ blocklib/blocks/throttle/throttle_cpu.cc
@@ -28,18 +28,12 @@ bool throttle_cpu::start()
 work_return_code_t throttle_cpu::work(std::vector<block_work_input_sptr>& work_input,
                                       std::vector<block_work_output_sptr>& work_output)
 {
-    // check for updated rx_rate tag
-    // if (!d_ignore_tags) {
-    // uint64_t abs_N = work_input[0]->n_items_read;
-    // std::vector<tag_t> all_tags;
-    // get_tags_in_range(all_tags, 0, abs_N, abs_N + noutput_items);
-    // for (const auto& tag : all_tags) {
-    //     if (pmt::eq(tag.key, throttle_rx_rate_pmt)) {
-    //         double new_rate = pmt::to_double(tag.value);
-    //         set_sample_rate(new_rate);
-    //     }
-    // }
-    // }
+    if (d_sleeping)
+    {
+        produce_each(0, work_output);
+        consume_each(0, work_input);
+        return work_return_code_t::WORK_OK;
+    }
 
     // copy all samples output[i] <= input[i]
     auto in = work_input[0]->items<uint8_t>();
@@ -55,27 +49,33 @@ work_return_code_t throttle_cpu::work(std::vector<block_work_input_sptr>& work_i
     if (expected_time > now) {
         auto limit_duration =
             std::chrono::duration<double>(std::numeric_limits<long>::max());
-        // if (expected_time - now > limit_duration) {
-        //     GR_LOG_ALERT(d_logger,
-        //                  "WARNING: Throttle sleep time overflow! You "
-        //                  "are probably using a very low sample rate.");
-        // }
 
-        // We should no longer block inside of a work function since it may not have its
-        // own thread
-        // This should only be done in its own thread
-        GR_LOG_DEBUG(this->debug_logger(), "Throttle sleeping {}", std::chrono::duration_cast<std::chrono::milliseconds>(expected_time - now).count());
-        std::this_thread::sleep_until(expected_time);
+        d_sleeping = true;
+        std::thread t([this, expected_time, now]() {
+            GR_LOG_DEBUG(
+                this->debug_logger(),
+                "Throttle sleeping {}",
+                std::chrono::duration_cast<std::chrono::milliseconds>(expected_time - now)
+                    .count());
+            std::this_thread::sleep_until(expected_time);
+            this->p_scheduler->push_message(
+                std::make_shared<scheduler_action>(scheduler_action_t::NOTIFY_INPUT));
+            this->d_sleeping = false;
+        });
+        t.detach();
 
-        // need a more intelligent solution to inform the scheduler
-        // to return at a certain time
-        // n = 0;
-        // d_total_samples -= noutput_items;
+        n = 0;
+        d_total_samples -= noutput_items;
+        produce_each(0, work_output);
+        consume_each(0, work_input);
+        return work_return_code_t::WORK_OK;
     }
 
     // TODO: blocks like throttle shouldn't need to do a memcpy, but this would have to be
     // fixed in the buffering model and a special port type
-    std::memcpy(out, in, n * work_output[0]->buffer->item_size());
+    if (n) {
+        std::memcpy(out, in, n * work_output[0]->buffer->item_size());
+    }
     work_output[0]->n_produced = n;
 
     GR_LOG_DEBUG(this->debug_logger(), "Throttle produced {}", n);
