f3c16749336bea45c0e18c30b065c7d77b603ff3
Josh Morman <jmorman@gnuradio.org>
2022-01-07 10:50:31 -0500

    wip: param changed to pmt
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- runtime/include/gnuradio/parameter.hh
+++ runtime/include/gnuradio/parameter.hh
@@ -14,122 +14,6 @@
 
 namespace gr {
 
-enum class param_flags_t {
-    NO_FLAGS = 0,
-    MANDATORY = 1 << 0, // used to indicate that this parameter must be set (if params are
-                        // removed from constructor)
-    CONST =
-        1 << 1, // set at initialization, but cannot be set once a flowgraph is running
-};
-
-class param
-{
-public:
-    typedef std::shared_ptr<param> sptr;
-    static sptr
-    make(const uint32_t id, const std::string name, pmtf::pmt pmt_value = nullptr)
-    {
-        return std::make_shared<param>(id, name, pmt_value);
-    }
-    param(const uint32_t id, const std::string name, pmtf::pmt pmt_value)
-        : _id(id), _name(name), _pmt_value(pmt_value)
-    {
-    }
-    virtual ~param() {}
-    std::string to_string() { return ""; };
-
-    const auto id() { return _id; }
-    const auto name() { return _name; }
-    const auto pmt_value() { return _pmt_value; }
-
-    void set_pmt_value(pmtf::pmt val) { _pmt_value = val; };
-
-
-protected:
-    const uint32_t _id;
-    const std::string _name;
-    param_type_t _type; // should be some sort of typeinfo, but worst case enum or string
-    pmtf::pmt _pmt_value;
-};
-
-template <class T>
-class scalar_param : public param
-{
-public:
-    typedef std::shared_ptr<scalar_param> sptr;
-    static sptr make(const uint32_t id, const std::string name, T value)
-    {
-        return std::make_shared<scalar_param<T>>(id, name, value);
-    }
-    scalar_param<T>(const uint32_t id, const std::string name, T value)
-        : param(id, name, pmtf::scalar<T>(value))
-    {
-    }
-    virtual ~scalar_param<T>() {}
-
-    void set_value(T val)
-    {
-        std::static_pointer_cast<pmtf::scalar<T>>(pmt_value())->set_pmt_value(val);
-    }
-    T value()
-    {
-        return pmtf::get_scalar<T>(pmt_value()).value();
-    }
-};
-
-template <class T>
-class vector_param : public param
-{
-public:
-    typedef std::shared_ptr<vector_param> sptr;
-    static sptr make(const uint32_t id, const std::string name, const std::vector<T>& value = {})
-    {
-        return std::make_shared<vector_param<T>>(id, name, value);
-    }
-    vector_param<T>(const uint32_t id, const std::string name, const std::vector<T>& value = {})
-        : param(id, name, pmtf::vector<T>(value))
-    {
-    }
-    virtual ~vector_param<T>() {}
-
-    void set_value(std::vector<T> val)
-    {
-        std::static_pointer_cast<pmtf::vector<T>>(pmt_value())->set_pmt_value(val);
-    }
-    std::vector<T> value()
-    {
-        return pmtf::get_vector<T>(pmt_value()).value();
-    }
-
-protected:
-    T _value;
-};
-
-class string_param : public param
-{
-public:
-    typedef std::shared_ptr<string_param> sptr;
-    static sptr make(const uint32_t id, const std::string name, const std::string& value)
-    {
-        return std::make_shared<string_param>(id, name, value);
-    }
-    string_param(const uint32_t id, const std::string name, const std::string& value)
-        : param(id, name, pmtf::string(value))
-    {
-    }
-    virtual ~string_param() {}
-
-    void set_value(const std::string& val)
-    {
-        set_pmt_value(pmtf::pmt(val));
-    }
-    std::string value()
-    {
-        return pmtf::get_string(pmt_value()).data();
-    }
-};
-
-
 class param_action
 {
 protected:
@@ -179,8 +63,6 @@ private:
 
 typedef std::queue<param_action_with_callback> param_action_queue;
 
-typedef std::shared_ptr<param> param_sptr;
-
 class param_query_action : public param_action_with_callback
 {
 public:
@@ -207,34 +89,22 @@ public:
 
 struct parameter_config
 {
-    std::vector<param_sptr> params;
-    size_t num() { return params.size(); }
-    void add(param_sptr b) { params.push_back(b); }
-    param_sptr get(const uint32_t id)
+    std::map<std::string, pmtf::pmt> param_map;
+    std::map<int, pmtf::pmt> param_map_int;
+    size_t num() { return param_map.size(); }
+    void add(const std::string& name, int id, pmtf::pmt b) { 
+        param_map[name] = b; 
+        param_map_int[id] = b;
+        }
+    pmtf::pmt get(const std::string& name)
     {
-        auto pred = [id](param_sptr item) { return item->id() == id; };
-        std::vector<param_sptr>::iterator it =
-            std::find_if(std::begin(params), std::end(params), pred);
-
-        if (it == std::end(params))
-            throw std::runtime_error(
-                "parameter not defined for this block"); // TODO logging
-
-        return *it;
+        return param_map[name];
     }
-    param_sptr get(const std::string& name)
+    pmtf::pmt get(int id)
     {
-        auto pred = [name](param_sptr item) { return item->name() == name; };
-        std::vector<param_sptr>::iterator it =
-            std::find_if(std::begin(params), std::end(params), pred);
-
-        if (it == std::end(params))
-            throw std::runtime_error(
-                "parameter not defined for this block"); // TODO logging
-
-        return *it;
+        return param_map_int[id];
     }
-    void clear() { params.clear(); }
+    void clear() { param_map.clear(); }
 };
 
 } // namespace gr
