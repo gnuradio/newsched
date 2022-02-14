948409f6dd13e53c38b5a66ed60d80342e67bc9a
Josh Morman <jmorman@gnuradio.org>
2022-02-14 17:38:55 -0500

    gr: allow connection of phantom nodes
    In cases where the node is not connected to anything, make it
    configurable to nullptrs via the graph::connect routine
    
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- gr/lib/graph.cc
+++ gr/lib/graph.cc
@@ -2,26 +2,32 @@
 
 namespace gr {
 
-edge_sptr graph::connect(const node_endpoint& src,
-                    const node_endpoint& dst)
+edge_sptr graph::connect(const node_endpoint& src, const node_endpoint& dst)
 {
-    
-    if (src.port()->itemsize() != dst.port()->itemsize() && src.port()->itemsize() > 0 && dst.port()->itemsize() > 0 )
-    {
+    if (src.port() && dst.port() && src.port()->itemsize() != dst.port()->itemsize() &&
+        src.port()->itemsize() > 0 && dst.port()->itemsize() > 0) {
         std::stringstream msg;
-        msg << "itemsize mismatch: " << src << " using " << src.port()->itemsize() << ", " << dst
-            << " using " << dst.port()->itemsize();
+        msg << "itemsize mismatch: " << src << " using " << src.port()->itemsize() << ", "
+            << dst << " using " << dst.port()->itemsize();
         throw std::invalid_argument(msg.str());
     }
 
     // If not untyped ports, check that data types are the same
     // TODO
-    
+
     auto newedge = edge::make(src, dst);
     _edges.push_back(newedge);
-    if (src.port()->type() == gr::port_type_t::STREAM)
-    {
+    if ((src.port() && src.port()->type() == gr::port_type_t::STREAM) ||
+        (dst.port() && dst.port()->type() == gr::port_type_t::STREAM)) {
         _stream_edges.push_back(newedge);
+    }
+
+    if (!src.node() || !dst.node())
+    {
+        if (src.node())
+            add_orphan_node(src.node());
+        if (dst.node())
+            add_orphan_node(dst.node());
     }
 
     _nodes = calc_used_nodes();
@@ -38,7 +44,7 @@ edge_sptr graph::connect(const node_endpoint& src,
         } else {
             cnt = name_count[b->name()];
         }
-        //b->set_alias(b->name() + std::to_string(cnt));
+        // b->set_alias(b->name() + std::to_string(cnt));
         name_count[b->name()] = cnt + 1;
 
         // for now, just use the name+nodeid as the alias
@@ -46,59 +52,84 @@ edge_sptr graph::connect(const node_endpoint& src,
     }
 
     // Give the underlying port objects information about the connected ports
-    src.port()->connect(dst.port());
-    dst.port()->connect(src.port());
+    if (src.port())
+        src.port()->connect(dst.port());
+    if (dst.port())
+        dst.port()->connect(src.port());
 
     return newedge;
 }
 
 edge_sptr graph::connect(node_sptr src_node,
-                    unsigned int src_port_index,
-                    node_sptr dst_node,
-                    unsigned int dst_port_index)
+                         unsigned int src_port_index,
+                         node_sptr dst_node,
+                         unsigned int dst_port_index)
 {
-    port_sptr src_port =
-        src_node->get_port(src_port_index, port_type_t::STREAM, port_direction_t::OUTPUT);
-    if (src_port == nullptr)
-        throw std::invalid_argument("Source Port not found");
+    port_sptr src_port = (src_node == nullptr)
+                             ? nullptr
+                             : src_node->get_port(src_port_index,
+                                                  port_type_t::STREAM,
+                                                  port_direction_t::OUTPUT);
+    // if (src_port == nullptr)
+    //     throw std::invalid_argument("Source Port not found");
 
-    port_sptr dst_port =
-        dst_node->get_port(dst_port_index, port_type_t::STREAM, port_direction_t::INPUT);
-    if (dst_port == nullptr)
-        throw std::invalid_argument("Destination port not found");
+    port_sptr dst_port = (dst_node == nullptr)
+                             ? nullptr
+                             : dst_node->get_port(dst_port_index,
+                                                  port_type_t::STREAM,
+                                                  port_direction_t::INPUT);
+    // if (dst_port == nullptr)
+    //     throw std::invalid_argument("Destination port not found");
 
-    return connect(node_endpoint(src_node, src_port),
-            node_endpoint(dst_node, dst_port));
+    return connect(node_endpoint(src_node, src_port), node_endpoint(dst_node, dst_port));
 }
 
 edge_sptr graph::connect(node_sptr src_node,
-                    node_sptr dst_node)
+                         const std::string& src_port_name,
+                         node_sptr dst_node,
+                         const std::string& dst_port_name)
+{
+        port_sptr src_port = (src_node == nullptr)
+                             ? nullptr : src_node->get_port(src_port_name);
+    // if (src_port == nullptr)
+        // throw std::invalid_argument("Source Port not found");
+
+        port_sptr dst_port = (dst_node == nullptr)
+                             ? nullptr : dst_node->get_port(dst_port_name);
+    // if (dst_port == nullptr)
+        // throw std::invalid_argument("Destination port not found");
+
+    return connect(node_endpoint(src_node, src_port), node_endpoint(dst_node, dst_port));
+}
+
+
+edge_sptr graph::connect(node_sptr src_node, node_sptr dst_node)
 {
     return connect(src_node, 0, dst_node, 0);
 }
 
-edge_sptr graph::connect(std::pair<node_sptr, unsigned int> src, std::pair<node_sptr, unsigned int> dst)
+edge_sptr graph::connect(std::pair<node_sptr, unsigned int> src,
+                         std::pair<node_sptr, unsigned int> dst)
 {
     return connect(src.first, src.second, dst.first, dst.second);
 }
 
-edge_sptr graph::connect(std::pair<node_sptr, const std::string&> src, std::pair<node_sptr, const std::string&> dst)
+edge_sptr graph::connect(std::pair<node_sptr, const std::string&> src,
+                         std::pair<node_sptr, const std::string&> dst)
 {
     return connect(src.first, src.second, dst.first, dst.second);
 }
 
 edge_vector_t graph::connect(const std::vector<std::pair<node_sptr, unsigned int>>& pairs)
 {
-    if (pairs.size() < 2)
-    {
+    if (pairs.size() < 2) {
         throw std::runtime_error("connect: must call with 2 or more node/index pairs");
     }
 
     edge_vector_t ret;
     auto last_node = pairs[0].first;
     auto last_index = pairs[0].second;
-    for (size_t i = 1; i < pairs.size(); i++)
-    {
+    for (size_t i = 1; i < pairs.size(); i++) {
         ret.push_back(connect(last_node, last_index, pairs[i].first, pairs[i].second));
         last_node = pairs[i].first;
         last_index = pairs[i].second;
@@ -109,41 +140,19 @@ edge_vector_t graph::connect(const std::vector<std::pair<node_sptr, unsigned int
 
 edge_vector_t graph::connect(const std::vector<node_sptr>& nodes)
 {
-    if (nodes.size() < 2)
-    {
+    if (nodes.size() < 2) {
         throw std::runtime_error("connect: must call with 2 or more nodes");
     }
 
     edge_vector_t ret;
     auto last_node = nodes[0];
-    for (size_t i = 1; i < nodes.size(); i++)
-    {
+    for (size_t i = 1; i < nodes.size(); i++) {
         ret.push_back(connect(last_node, nodes[i]));
         last_node = nodes[i];
     }
 
     return ret;
 }
-
-edge_sptr graph::connect(node_sptr src_node,
-                    const std::string& src_port_name,
-                    node_sptr dst_node,
-                    const std::string& dst_port_name)
-{
-    port_sptr src_port =
-        src_node->get_port(src_port_name);
-    if (src_port == nullptr)
-        throw std::invalid_argument("Source Port not found");
-
-    port_sptr dst_port =
-        dst_node->get_port(dst_port_name);
-    if (dst_port == nullptr)
-        throw std::invalid_argument("Destination port not found");
-
-    return connect(node_endpoint(src_node, src_port),
-            node_endpoint(dst_node, dst_port));
-}
-
 
 void graph::add_orphan_node(node_sptr orphan_node)
 {
@@ -160,6 +169,21 @@ node_vector_t graph::calc_used_nodes()
             tmp.push_back(p->src().node());
         if (p->dst().node())
             tmp.push_back(p->dst().node());
+    }
+    for (auto n : _orphan_nodes) {
+        tmp.push_back(n);
+    }
+
+    return unique_vector<node_sptr>(tmp);
+}
+
+node_vector_t graph::all_nodes()
+{
+    node_vector_t tmp;
+
+    // Collect all blocks in the edge list
+    for (auto& n : nodes()) {
+        tmp.push_back(n);
     }
     for (auto n : _orphan_nodes) {
         tmp.push_back(n);
@@ -192,8 +216,7 @@ void graph::add_edge(edge_sptr edge)
     // TODO: check that edge is not already in the graph
     _edges.push_back(edge);
     if ((edge->src().port() && edge->src().port()->type() == gr::port_type_t::STREAM) ||
-        (edge->dst().port() && edge->dst().port()->type() == gr::port_type_t::STREAM))
-    {
+        (edge->dst().port() && edge->dst().port()->type() == gr::port_type_t::STREAM)) {
         _stream_edges.push_back(edge);
     }
 }
