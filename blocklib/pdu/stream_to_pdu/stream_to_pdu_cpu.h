148e14423646721958b9378e15a1de51fecb0ec8
Josh Morman <jmorman@gnuradio.org>
2022-04-26 15:47:08 -0400

    pdu: add pdu module
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

---
+++ blocklib/pdu/stream_to_pdu/stream_to_pdu_cpu.h
@@ -0,0 +1,33 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2022 Josh Morman
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
+#pragma once
+
+#include <gnuradio/pdu/stream_to_pdu.h>
+
+namespace gr {
+namespace pdu {
+
+template <class T>
+class stream_to_pdu_cpu : public stream_to_pdu<T>
+{
+public:
+    stream_to_pdu_cpu(const typename stream_to_pdu<T>::block_args& args);
+    
+    virtual work_return_code_t work(std::vector<block_work_input_sptr>& work_input,
+                                    std::vector<block_work_output_sptr>& work_output) override;
+
+private:
+    size_t d_packet_len;
+};
+
+
+} // namespace pdu
+} // namespace gr
