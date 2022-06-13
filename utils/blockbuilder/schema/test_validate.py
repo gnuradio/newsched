152727eb20fbd2d08859d071673fd4b942cfca3b
Josh Morman <jmorman@gnuradio.org>
2022-06-13 14:04:04 -0400

    blockbuilder: populate port parameters with defaults
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- utils/blockbuilder/schema/test_validate.py
+++ utils/blockbuilder/schema/test_validate.py
@@ -1,6 +1,7 @@
 import json
 import jsonschema
 from jsonschema import validate
+from regex import D
 import yaml
 import os
 
@@ -18,13 +19,15 @@ filedir = os.path.dirname(__file__)
 with open (os.path.join(filedir, '../../../blocklib/blocks/vector_sink/vector_sink.yml')) as f:
     d = yaml.safe_load(f)
 
-schema_filename = os.path.join(filedir, 'gnuradio-block-schema.json')
+schema_filename = os.path.join(filedir, 'gnuradio-block.json')
 with open(schema_filename,'r') as f:
     schema = json.load(f)
     
 
 isvalid = validate_json(d, schema)
+isvalid = validate_json(d['ports'][0], schema['definitions']['Port'])
 
 default_obj = jsonschema_default.create_from(schema_filename)
+default_obj.update(d)
 
 x = 3
