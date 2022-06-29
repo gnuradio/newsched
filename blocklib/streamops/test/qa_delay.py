3e516641981fcffccc74020cd9a3f3f1f3623c89
Josh Morman <jmorman@gnuradio.org>
2022-06-29 15:48:17 -0400

    blocks/delay: propagate tags
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- blocklib/streamops/test/qa_delay.py
+++ blocklib/streamops/test/qa_delay.py
@@ -10,18 +10,16 @@
 
 
 from gnuradio import gr, gr_unittest, blocks, streamops
-# import pmt
+import pmtf
 
 
 class test_delay(gr_unittest.TestCase):
 
     def setUp(self):
         self.tb = gr.flowgraph()
-        self.rt = gr.runtime()
 
     def tearDown(self):
         self.tb = None
-        self.rt = None
 
     def test_000(self):
         delta_t = 0
@@ -35,8 +33,7 @@ class test_delay(gr_unittest.TestCase):
 
         tb.connect(src, op)
         tb.connect(op, dst)
-        self.rt.initialize(self.tb)
-        self.rt.run()
+        tb.run()
         dst_data = dst.data()
         self.assertEqual(expected_result, dst_data)
 
@@ -52,35 +49,53 @@ class test_delay(gr_unittest.TestCase):
 
         tb.connect(src, op)
         tb.connect(op, dst)
-        self.rt.initialize(self.tb)
-        self.rt.run()
+        tb.run()
         dst_data = dst.data()
         self.assertEqual(expected_result, dst_data)
 
-    # def test_020(self):
-    #     tb = self.tb
-    #     vector_sink = blocks.vector_sink_f(1)
-    #     ref_sink = blocks.vector_sink_f(1)
-    #     tags_strobe = blocks.tags_strobe(
-    #         gr.sizeof_float * 1,
-    #         pmt.intern("TEST"),
-    #         100,
-    #         pmt.intern("strobe"))
-    #     head = streamops.head(gr.sizeof_float * 1, 10**5)
-    #     delay = streamops.delay(gr.sizeof_float * 1, 100)
-    #     tb.connect((delay, 0), (head, 0))
-    #     tb.connect((head, 0), (vector_sink, 0))
-    #     tb.connect((tags_strobe, 0), (delay, 0))
-    #     tb.connect((tags_strobe, 0), (ref_sink, 0))
-    #     tb.run()
 
-    #     tags = vector_sink.tags()
-    #     self.assertNotEqual(len(tags), 0)
-    #     lastoffset = tags[0].offset - 100
-    #     for tag in tags:
-    #         newoffset = tag.offset
-    #         self.assertEqual(newoffset, lastoffset + 100)
-    #         lastoffset = newoffset
+    def test_011(self):
+        delta_t = 3
+        tb = self.tb
+        src_data = [1.0+float(x) for x in range(0, 100)]
+        expected_result = list(delta_t * [0.0] + src_data)
+
+        src = blocks.vector_source_f(src_data)
+        op = streamops.delay(0)
+        op.set_dly(delta_t)
+        dst = blocks.vector_sink_f()
+
+        tb.connect(src, op)
+        tb.connect(op, dst)
+        tb.run()
+        dst_data = dst.data()
+        self.assertEqual(expected_result, dst_data)
+
+    def test_020(self):
+        nsamps = 123
+        tb = self.tb
+        vector_sink = blocks.vector_sink_f(1)
+        ref_sink = blocks.vector_sink_f(1)
+        tags_strobe = blocks.tags_strobe(
+            pmtf.pmt("TEST"),
+            nsamps,
+            "strobe",
+            gr.sizeof_float)
+        head = streamops.head(10**5)
+        delay = streamops.delay(100)
+        tb.connect((delay, 0), (head, 0))
+        tb.connect((head, 0), (vector_sink, 0))
+        tb.connect((tags_strobe, 0), (delay, 0))
+        tb.connect((tags_strobe, 0), (ref_sink, 0))
+        tb.run()
+
+        tags = vector_sink.tags()
+        self.assertNotEqual(len(tags), 0)
+        lastoffset = tags[0].offset() - nsamps
+        for tag in tags:
+            newoffset = tag.offset()
+            self.assertEqual(newoffset, lastoffset + nsamps)
+            lastoffset = newoffset
 
 
 if __name__ == '__main__':
