0f6b75850cf70807e67c4b874b74d133bdf6d045
Josh Morman <jmorman@gnuradio.org>
2022-07-01 10:35:17 -0400

    gr: work_io using objects instead of sptr vec
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- gr/include/gnuradio/block_work_io.h
+++ gr/include/gnuradio/block_work_io.h
@@ -20,7 +20,7 @@ struct block_work_input {
     size_t n_consumed =
         0; // output the number of items that were consumed on the work() call
     port_sptr port = nullptr;
-    block_work_input(int n_items_, buffer_reader_sptr p_buf_, port_sptr p=nullptr)
+    block_work_input(int n_items_, buffer_reader_sptr p_buf_, port_sptr p = nullptr)
         : n_items(n_items_), buffer(p_buf_), port(p)
     {
     }
@@ -79,7 +79,7 @@ struct block_work_output {
         0; // output the number of items that were produced on the work() call
     port_sptr port = nullptr;
 
-    block_work_output(int _n_items, buffer_sptr p_buf_, port_sptr p=nullptr)
+    block_work_output(int _n_items, buffer_sptr p_buf_, port_sptr p = nullptr)
         : n_items(_n_items), buffer(p_buf_), port(p)
     {
     }
@@ -142,45 +142,107 @@ enum class work_return_code_t {
         1, /// rather than blocking in the work function, the block will call back to the
            /// parent interface when it is ready to be called again
 };
+template <typename T>
+class io_vec_wrap
+{
+private:
+    std::vector<T> _vec;
+    std::map<std::string, T*> _map;
 
+public:
+    T& operator[](size_t idx) { return _vec[idx]; }
+    T& operator[](const std::string& name) { return *_map[name]; }
+    auto begin() { return _vec.begin(); }
+    auto end() { return _vec.end(); }
+    auto back() { return _vec.back(); }
+    auto size() { return _vec.size(); }
+    auto empty() { return _vec.empty(); }
+    auto clear()
+    {
+        _vec.clear();
+        _map.clear();
+    }
+    void push_back(const T& element) { _vec.push_back(element); }
+    void add_to_map(size_t index, const std::string& name)
+    {
+        _map[name] = &(_vec[index]);
+    }
+};
 
 class work_io
 {
 public:
-    friend class block;
-    size_t nin() { return _inputs.size(); }
-    size_t nout() { return _outputs.size(); }
-    block_work_input_sptr& input(size_t idx)
-    {
-        return _inputs[idx];
-    }
-    block_work_input_sptr& input(const std::string&);
-    block_work_output_sptr& output(size_t idx)
-    {
-        return _outputs[idx];
-    }
-    block_work_output_sptr& output(const std::string&);
-    std::vector<block_work_input_sptr>& inputs() { return _inputs; }
-    std::vector<block_work_output_sptr>& outputs() { return _outputs; }
-    // block_work_input_sptr& operator[](size_t);
-    // block_work_input_sptr& operator[](const std::string&);
+    work_io() {}
+    work_io(const work_io&) = delete;
+    work_io& operator=(const work_io&) = delete;
 
-    void consume_each(size_t n_items) {
+    io_vec_wrap<block_work_input>& inputs() { return _inputs; }
+    io_vec_wrap<block_work_output>& outputs() { return _outputs; }
+    void clear()
+    {
+        _inputs.clear();
+        _outputs.clear();
+    }
+    void reset()
+    {
+        for (auto& w : _inputs) {
+            w.reset();
+        }
+        for (auto& w : _outputs) {
+            w.reset();
+        }
+    }
+    // Convenience Methods
+    void consume_each(size_t n_items)
+    {
         for (auto& w : inputs()) {
-            w->n_consumed = n_items;
+            w.n_consumed = n_items;
         }
     }
-    void produce_each(size_t n_items) {
+    void produce_each(size_t n_items)
+    {
         for (auto& w : outputs()) {
-            w->n_produced = n_items;
+            w.n_produced = n_items;
         }
     }
-private:
-    std::vector<block_work_input_sptr> _inputs;
-    std::vector<block_work_output_sptr> _outputs;
+    size_t min_noutput_items()
+    {
+        auto result = (std::min_element(
+            _outputs.begin(),
+            _outputs.end(),
+            [](const block_work_output& lhs, const block_work_output& rhs) {
+                return (lhs.n_items < rhs.n_items);
+            }));
+        return (*result).n_items;
+    }
 
-    std::map<std::string, block_work_input_sptr> _input_name_map;
-    std::map<std::string, block_work_output_sptr> _output_name_map;
+    size_t min_ninput_items()
+    {
+        auto result = (std::min_element(
+            _inputs.begin(),
+            _inputs.end(),
+            [](const block_work_input& lhs, const block_work_input& rhs) {
+                return (lhs.n_items < rhs.n_items);
+            }));
+        return (*result).n_items;
+    }
+
+private:
+    friend class block;
+    io_vec_wrap<block_work_input> _inputs;
+    io_vec_wrap<block_work_output> _outputs;
+
+    void add_input(port_sptr p)
+    {
+        _inputs.push_back(block_work_input(0, p->buffer_reader(), p));
+        _inputs.add_to_map(_inputs.size() - 1, p->name());
+    }
+
+    void add_output(port_sptr p)
+    {
+        _outputs.push_back(block_work_output(0, p->buffer(), p));
+        _outputs.add_to_map(_outputs.size() - 1, p->name());
+    }
 };
 
 } // namespace gr
