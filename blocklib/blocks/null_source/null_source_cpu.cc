281d815ebac7c20d155c7a11c83eb11da0820a60
Josh Morman <jmorman@gnuradio.org>
2022-07-05 08:11:29 -0400

    blocks: iterate more conveniently over outputs
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- blocklib/blocks/null_source/null_source_cpu.cc
+++ blocklib/blocks/null_source/null_source_cpu.cc
@@ -17,12 +17,10 @@ null_source_cpu::null_source_cpu(block_args args) : INHERITED_CONSTRUCTORS {}
 work_return_code_t null_source_cpu::work(work_io& wio)
 {
     auto itemsize = wio.outputs()[0].buffer->item_size();
-    void* optr;
-    for (size_t n = 0; n < wio.outputs().size(); n++) {
-        optr = wio.outputs()[n].items<void>();
-        auto noutput_items = wio.outputs()[n].n_items;
-        memset(optr, 0, noutput_items * itemsize);
-        wio.outputs()[n].n_produced = noutput_items;
+    for (auto& out : wio.outputs()) {
+        auto noutput_items = out.n_items;
+        memset(out.raw_items(), 0, noutput_items * itemsize);
+        out.n_produced = noutput_items;
     }
 
     return work_return_code_t::WORK_OK;
